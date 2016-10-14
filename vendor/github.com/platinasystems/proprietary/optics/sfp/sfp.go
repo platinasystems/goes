96c8da2a9909d724dff31a36764b16f390521efc
Tom Grennan <tgrennan@platinasystems.com>
2016-10-14 11:24:09 -0700

    punt on binary-only-package build of vnetdevices
    for now...
    
    Signed-off-by: Tom Grennan <tgrennan@platinasystems.com>

--- vendor/github.com/platinasystems/proprietary/optics/sfp/sfp.go
+++
@@ -1,7 +0,0 @@
-// Copyright 2015-2016 Platina Systems, Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style license described in the
-// LICENSE file.
-
-//go:binary-only-package
-
-package sfp
