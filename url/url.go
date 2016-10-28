1d2791120c22730e7ced1b82805ff456df9146c9
Kevin Paul Herbert <kph@platinasystems.com>
2016-10-28 10:52:42 -0700

    wget: Factor function that gets files
    Create FetchReqs helper function to get a batch of files
    in parallel. Factor this out of the wget commad, and call
    it from wget.
    
    Signed-off-by: Kevin Paul Herbert <kph@platinasystems.com>
    Signed-off-by: Tom Grennan <tgrennan@platinasystems.com>

--- url/url.go
+++ url/url.go
@@ -13,6 +13,9 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"time"
+
+	"github.com/cavaliercoder/grab"
 )
 
 func Open(path string) (io.ReadCloser, error) {
@@ -105,3 +108,75 @@ func create(path string, isAppend bool) (io.WriteCloser, error) {
 
 func Create(path string) (io.WriteCloser, error) { return create(path, false) }
 func Append(path string) (io.WriteCloser, error) { return create(path, true) }
+
+func FetchReqs(parallel int, reqs []*grab.Request) (successes int, err error) {
+	successes = 0
+	firstErr := error(nil)
+
+	// create a custom client
+	client := grab.NewClient()
+	client.UserAgent = "Platina Go-ES"
+
+	// start file downloads at the requested batch size
+	fmt.Printf("Downloading %d files...\n", len(reqs))
+	respch := client.DoBatch(parallel, reqs...)
+
+	// start a ticker to update progress every 200ms
+	t := time.NewTicker(200 * time.Millisecond)
+
+	// monitor downloads
+	completed := 0
+	inProgress := 0
+	responses := make([]*grab.Response, 0)
+	for completed < len(reqs) {
+		select {
+		case resp := <-respch:
+			// a new response has been received and has started downloading
+			// (nil is received once, when the channel is closed by grab)
+			if resp != nil {
+				responses = append(responses, resp)
+			}
+
+		case <-t.C:
+			// clear lines
+			if inProgress > 0 {
+				fmt.Printf("\033[%dA\033[K", inProgress)
+			}
+
+			// update completed downloads
+			for i, resp := range responses {
+				if resp != nil && resp.IsComplete() {
+					//Link request to response
+					resp.Request.Tag = resp
+					// print final result
+					if resp.Error != nil {
+						fmt.Fprintf(os.Stderr, "Error downloading %s: %v\n", resp.Request.URL(), resp.Error)
+						if firstErr == nil {
+							firstErr = resp.Error
+						}
+					} else {
+						fmt.Printf("Finished %s %d / %d bytes (%d%%)\n", resp.Filename, resp.BytesTransferred(), resp.Size, int(100*resp.Progress()))
+						successes++
+					}
+
+					// mark completed
+					responses[i] = nil
+					completed++
+				}
+			}
+
+			// update downloads in progress
+			inProgress = 0
+			for _, resp := range responses {
+				if resp != nil {
+					inProgress++
+					fmt.Printf("Downloading %s %d / %d bytes (%d%%)\033[K\n", resp.Filename, resp.BytesTransferred(), resp.Size, int(100*resp.Progress()))
+				}
+			}
+		}
+	}
+
+	t.Stop()
+
+	return successes, firstErr
+}
