e226bbddca5f11194d657e7538a0ddb15cb50255
Kevin Paul Herbert <kph@platinasystems.com>
2016-12-29 09:37:55 -0800

    linux_file_load.go: Add an actual file load method
    For architectures other than x86-64, add support for the segment-based
    kernel loader.
    
    Signed-off-by: Kevin Paul Herbert <kph@platinasystems.com>

--- internal/kexec/linux_file_load.go
+++ internal/kexec/linux_file_load.go
@@ -4,9 +4,70 @@
 package kexec
 
 import (
+	"github.com/platinasystems/go/internal/fdt"
+	"fmt"
+	"io/ioutil"
+	"github.com/platinasystems/go/internal/memmap"
 	"os"
+	"syscall"
 )
 
+func fileAddSegment(s []KexecSegment, f *os.File, a uintptr) ([]KexecSegment, error) {
+	if f != nil {
+		dat, err := ioutil.ReadAll(f)
+		if err != nil {
+			return nil, err
+		}
+		return SliceAddSegment(s, &dat, a), nil
+	}
+	return s, nil
+}
+
+func fileLoadFiles(k *os.File, i *os.File, cmdline string, flags uintptr) (err error) {
+	m, err := memmap.FileToMap("/proc/iomem")
+	if err != nil {
+		return err
+	}
+
+	kCode, ok := m["Kernel code"]
+	if !ok {
+		return fmt.Errorf("Can't find kernel code in /proc/iomem")
+	}
+	kBase := kCode.Ranges[0].Start
+	segments := make([]KexecSegment, 0, 3)
+
+	segments, err = fileAddSegment(segments, k, kBase)
+	if err != nil {
+		return err
+	}
+	segments, err = fileAddSegment(segments, i, kBase + 0x2000000)
+	if err != nil {
+		return err
+	}
+	t := fdt.DefaultTree()
+	if t != nil {
+		chosen := t.RootNode.Children["chosen"]
+		if chosen == nil {
+			chosen = &fdt.Node{ Name: "chosen", Depth: 1}
+			t.RootNode.Children["chosen"] = chosen
+		}
+		//need 64 bit support - parsing for #address-cells
+		if i != nil {
+			chosen.Properties["linux,initrd-start"] = t.PropUint32ToSlice(uint32(segments[1].Mem))
+			chosen.Properties["linux,initrd-end"] = t.PropUint32ToSlice(uint32(segments[1].Mem) + uint32(segments[1].Bufsz) + 1)
+		}
+		chosen.Properties["bootargs"] = []byte(cmdline + "\x00")
+		fdt := t.FlattenTreeToSlice()
+		segments = SliceAddSegment(segments, &fdt, kBase + 0x1000000)
+	}
+	err = SegmentLoad(uint64(kBase), &segments, 0)
+	return err
+}
+
 func FileLoad(k *os.File, i *os.File, cmdline string, flags uintptr) (err error) {
-	return fileLoadSyscall(k, i, cmdline, flags)
+	err = fileLoadSyscall(k, i, cmdline, flags)
+	if err == syscall.ENOSYS {
+		err = fileLoadFiles(k, i, cmdline, flags)
+	}
+	return err
 }
