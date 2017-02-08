bcc2211ce89bb3afd90429661bac735b79976c60
Eliot Dresselhaus <edressel@platinasystems.com>
2017-02-08 18:09:01 +0100

    ip6 <-> ip prefix
    Signed-off-by: Eliot Dresselhaus <edressel@platinasystems.com>

--- vnet/ip6/fib.go
+++ vnet/ip6/fib.go
@@ -4,6 +4,10 @@
 
 package ip6
 
+import (
+	"github.com/platinasystems/go/vnet/ip"
+)
+
 type Prefix struct {
 	Address
 	Len uint32
@@ -12,5 +16,16 @@ type Prefix struct {
 func (p *Prefix) SetLen(l uint) { p.Len = uint32(l) }
 func (a *Address) toPrefix() (p Prefix) {
 	p.Address = *a
+	return
+}
+
+func FromIp6Prefix(i *ip.Prefix) (p Prefix) {
+	copy(p.Address[:], i.Address[:AddressBytes])
+	p.Len = i.Len
+	return
+}
+func (p *Prefix) ToIpPrefix() (i ip.Prefix) {
+	copy(i.Address[:], p.Address[:])
+	i.Len = p.Len
 	return
 }
