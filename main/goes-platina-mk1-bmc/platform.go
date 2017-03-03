1906ecbe071acc7fc68721c4b6b94d7e1694def5
Mark Pleso <mpleso@platinasystems.com>
2017-03-03 12:09:45 -0800

    Fixed fsp550 admin state
    Signed-off-by: Mark Pleso <mpleso@platinasystems.com>

--- main/goes-platina-mk1-bmc/platform.go
+++ main/goes-platina-mk1-bmc/platform.go
@@ -5,11 +5,17 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+
 	"github.com/platinasystems/go/internal/environ/fantray"
 	"github.com/platinasystems/go/internal/environ/fsp"
 	"github.com/platinasystems/go/internal/environ/nuvoton"
 	"github.com/platinasystems/go/internal/environ/nxp"
 	"github.com/platinasystems/go/internal/environ/ti"
+	"github.com/platinasystems/go/internal/fdt"
+	"github.com/platinasystems/go/internal/fdtgpio"
+	"github.com/platinasystems/go/internal/gpio"
 )
 
 type platform struct {
@@ -28,6 +34,21 @@ func (p *platform) Init() (err error) {
 }
 
 func (p *platform) boardInit() (err error) {
+	gpio.File = "/boot/platina-mk1-bmc.dtb"
+	gpio.Aliases = make(gpio.GpioAliasMap)
+	gpio.Pins = make(gpio.PinMap)
+
+	// Parse linux.dtb to generate gpio map for this machine
+	if b, err := ioutil.ReadFile(gpio.File); err == nil {
+		t := &fdt.Tree{Debug: false, IsLittleEndian: false}
+		t.Parse(b)
+
+		t.MatchNode("aliases", fdtgpio.GatherAliases)
+		t.EachProperty("gpio-controller", "", fdtgpio.GatherPins)
+	} else {
+		return fmt.Errorf("%s: %v", gpio.File, err)
+	}
+
 	return nil
 }
 
