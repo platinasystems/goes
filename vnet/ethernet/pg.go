5f3dd698f586a271f94d5ebdd86e039dad1366aa
Eliot Dresselhaus <edressel@platinasystems.com>
2017-03-29 12:06:04 +0200

    * ethernet: parse vlan headers * ethernet pg: support generating 0 1 or 2 vlan tags in packets.
    Signed-off-by: Eliot Dresselhaus <edressel@platinasystems.com>

--- vnet/ethernet/pg.go
+++ vnet/ethernet/pg.go
@@ -34,22 +34,46 @@ func (m *pgMain) initTypes() {
 	m.typeMap[IP4.FromHost()] = pg.GetStreamType(m.v, "ip4")
 }
 
-var defaultHeader = Header{
-	Type: IP4.FromHost(),
-	Src:  Address{0xe0, 0xe1, 0xe2, 0xe3, 0xe4, 0xe5},
-	Dst:  Address{0xea, 0xeb, 0xec, 0xed, 0xee, 0xef},
-}
-
 func (m *pgMain) ParseStream(in *parse.Input) (r pg.Streamer, err error) {
 	m.initTypes()
 
 	var s pgStream
-	h := defaultHeader
 	for !in.End() {
+		var h struct {
+			v []VlanHeader
+			h Header
+		}
 		switch {
-		case in.Parse("%v", &h):
-			s.h = append(s.h, &h)
-			if t, ok := m.typeMap[h.Type]; ok {
+		case in.Parse("%v", &h.h):
+			for {
+				var v VlanHeader
+				if in.Parse("%v", &v) {
+					h.v = append(h.v, v)
+				} else {
+					break
+				}
+			}
+
+			inner_type := h.h.Type
+			switch len(h.v) {
+			case 0:
+			case 1:
+				h.v[0].Type = inner_type
+				h.h.Type = VLAN.FromHost()
+			case 2:
+				h.v[1].Type = inner_type
+				h.v[0].Type = VLAN.FromHost()
+				h.h.Type = VLAN_IN_VLAN.FromHost()
+			case 3:
+				err = fmt.Errorf("number of vlans must be <= 2, given %d", len(h.v))
+				return
+			}
+
+			s.h = append(s.h, &h.h)
+			for i := range h.v {
+				s.h = append(s.h, &h.v[i])
+			}
+			if t, ok := m.typeMap[inner_type]; ok {
 				var sub_r pg.Streamer
 				sub_r, err = t.ParseStream(in)
 				if err != nil {
