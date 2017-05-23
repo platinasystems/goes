cb9725849f22b3473c7c43a39b49e7647e74ea71
Eliot Dresselhaus <edressel@platinasystems.com>
2017-05-23 09:12:24 +0200

    rework header interface
    Signed-off-by: Eliot Dresselhaus <edressel@platinasystems.com>

--- vnet/header.go
+++ vnet/header.go
@@ -5,7 +5,6 @@
 package vnet
 
 import (
-	"bytes"
 	"fmt"
 	"unsafe"
 )
@@ -14,34 +13,37 @@ type PacketHeader interface {
 	// Number of packet bytes in this layer's payload.
 	Len() uint
 
-	// Finalize this layer given finalized inner layers.
-	// This allows, for example, the IP4 layer to compute length and checksum based on payload.
-	Finalize([]PacketHeader)
+	// Write this layer's packet data to given slice.
+	Write([]byte)
+
+	// Return given slice as packet header type.
+	Read([]byte) PacketHeader
 
 	String() string
-
-	// Append this layer's packet data to given buffer.
-	Write(*bytes.Buffer)
-
-	Read([]byte) PacketHeader
 }
 
 func Pointer(b []byte) unsafe.Pointer { return unsafe.Pointer(&b[0]) }
 
-func MakePacket(args ...PacketHeader) []byte {
-	n := len(args)
-	if n == 0 {
-		return nil
+func MakePacket(args ...PacketHeader) (b []byte) {
+	na := len(args)
+
+	// Find total length of packet.
+	l := uint(0)
+	for _, a := range args {
+		l += a.Len()
 	}
-	for i := 0; i < n; i++ {
-		args[n-1-i].Finalize(args[n-i:])
+
+	// Allocate buffer.
+	b = make([]byte, l)
+
+	// Write packet from innermost header to outermost.
+	k := uint(0)
+	for i := 0; i < na; i++ {
+		a := args[na-1-i]
+		k += a.Len()
+		a.Write(b[l-k:])
 	}
-	b := new(bytes.Buffer)
-	for i := 0; i < n; i++ {
-		b.Grow(int(args[i].Len()))
-		args[i].Write(b)
-	}
-	return b.Bytes()
+	return
 }
 
 func ReadPacket(b []byte, args ...PacketHeader) (hs []PacketHeader) {
@@ -61,9 +63,9 @@ func (i *IncrementingPayload) Len() uint                 { return i.Count }
 func (i *IncrementingPayload) Finalize(l []PacketHeader) {}
 func (i *IncrementingPayload) String() string            { return fmt.Sprintf("incrementing %d", i.Count) }
 
-func (i *IncrementingPayload) Write(b *bytes.Buffer) {
+func (i *IncrementingPayload) Write(b []byte) {
 	for j := uint(0); j < i.Count; j++ {
-		b.WriteByte(byte(j % 256))
+		b[j] = byte(j % 256)
 	}
 }
 func (i *IncrementingPayload) Read(b []byte) PacketHeader { return i }
