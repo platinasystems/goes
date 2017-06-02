7a5bf82aef6cebe764e6a431e5d5d1681832e27e
Tom Grennan <tgrennan@platinasystems.com>
2017-06-02 11:54:52 -0700

    implement redisd.Info("server")
    Signed-off-by: Tom Grennan <tgrennan@platinasystems.com>

--- goes/cmd/redisd/info.go
+++ goes/cmd/redisd/info.go
@@ -7,11 +7,37 @@ package redisd
 import (
 	"fmt"
 	"io"
+	"math"
+	"os"
+	"runtime"
+	"time"
+
+	. "github.com/platinasystems/go"
+	"github.com/platinasystems/go/internal/proc"
 )
 
+const SizeofInt = (32 << (^uint(0) >> 63)) >> 3
+
 func (redisd *Redisd) infoServer(w io.Writer) error {
-	_, err := fmt.Fprintln(w, "FIXME")
-	return err
+	var stat proc.Stat
+	if f, err := os.Open("/proc/self/stat"); err != nil {
+		return err
+	} else {
+		err = stat.ReadFrom(f)
+		f.Close()
+		if err != nil {
+			return err
+		}
+	}
+	fmt.Fprintln(w, "redis_git_sha1:", Package["version"])
+	fmt.Fprintln(w, "redis_git_dirty:", len(Package["diff"]) > 0)
+	fmt.Fprintln(w, "os:", runtime.GOOS)
+	fmt.Fprintln(w, "arch_bits:", SizeofInt*8)
+	fmt.Fprintln(w, "process_id:", stat.Pid)
+	seconds := time.Now().Sub(stat.StartTime).Seconds()
+	fmt.Fprintln(w, "uptime_in_seconds:", math.Floor(seconds+.5))
+	fmt.Fprintln(w, "uptime_in_days:", math.Floor((seconds/(60*60*24))+.5))
+	return nil
 }
 
 func (redisd *Redisd) infoClients(w io.Writer) error {
