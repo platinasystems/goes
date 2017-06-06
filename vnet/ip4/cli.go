ecfd4c410865968571e54b193d987783f60b82f6
Stig Thormodsrud <stig@platinasystems.com>
2017-06-05 17:26:05 -0700

    Fix issue 56: "vnet show ip fib summary" crashes with multiple tables
    - Check for nil
    
    Signed-off-by: Stig Thormodsrud <stig@platinasystems.com>

--- vnet/ip4/cli.go
+++ vnet/ip4/cli.go
@@ -55,7 +55,9 @@ func (m *Main) showIpFib(c cli.Commander, w cli.Writer, in *cli.Input) (err erro
 		fmt.Fprintf(w, "%6s%12s\n", "Table", "Routes")
 		for fi := range m.fibs {
 			fib := m.fibs[fi]
-			fmt.Fprintf(w, "%6d%12d\n", fi, fib.Len())
+			if fib != nil {
+				fmt.Fprintf(w, "%6d%12d\n", fi, fib.Len())
+			}
 		}
 		u := m.GetAdjacencyUsage()
 		fmt.Fprintf(w, "Adjacencies: heap %d used, %d free\n", u.Used, u.Free)
