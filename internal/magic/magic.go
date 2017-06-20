ae8d8d21acc009119ad4fa3a0bfe77db93f7b47c
Kevin Paul Herbert <kph@platinasystems.com>
2017-06-20 12:21:44 -0700

    magic.go: Template for a magic number parser
    Just something to get off the ground.
    
    Signed-off-by: Kevin Paul Herbert <kph@platinasystems.com>

---
+++ internal/magic/magic.go
@@ -0,0 +1,17 @@
+// Copyright © 2017 Platina Systems, Inc. All rights reserved.
+// Use of this source code is governed by the GPL-2 license described in the
+// LICENSE file.
+
+package magic
+
+func IdentifyPartitionMap(sniff []byte) string {
+	return ""
+}
+
+func IdentifyPartition(sniff []byte) string {
+	return ""
+}
+
+func IdentifyFile(sniff []byte) string {
+	return ""
+}
