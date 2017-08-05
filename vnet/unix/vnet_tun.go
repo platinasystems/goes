fc7c103ecbbeb38d75fd1b49dd863a5ebe96e6c6
Eliot Dresselhaus <edressel@platinasystems.com>
2017-08-05 09:00:04 +0200

    Signed-off-by: Eliot Dresselhaus <edressel@platinasystems.com>

--- vnet/unix/vnet_tun.go
+++ vnet/unix/vnet_tun.go
@@ -51,3 +51,7 @@ func (m *vnet_tun_main) create_tun(ns *net_namespace) (intf *tuntap_interface) {
 	ns.vnet_tun_interface = intf
 	return
 }
+
+func IsVnetTun(v *vnet.Vnet, si vnet.Si) bool {
+	return GetMain(v).si_is_vnet_tun(si)
+}
