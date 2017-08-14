67ed8c35ebeea1c697b48aca1bfb687864c4e53b
Stig Thormodsrud <stig@platinasystems.com>
2017-08-14 15:36:17 -0700

    use ip batch mode to delete interfaces
    Signed-off-by: Stig Thormodsrud <stig@platinasystems.com>

--- main/goes-platina-mk1/stop.go
+++ main/goes-platina-mk1/stop.go
@@ -27,12 +27,31 @@ func (p *mk1Main) stopHook(i *vnetd.Info, v *vnet.Vnet) error {
 		if err != nil {
 			return err
 		}
+		cmd := exec.Command("ip", "-batch", "-")
+		w, err := cmd.StdinPipe()
+		if err != nil {
+			return err
+		}
+		if err = cmd.Start(); err != nil {
+			return err
+		}
+		defer func() {
+			w.Close()
+			xerr := cmd.Wait()
+			if ee, found := xerr.(*exec.ExitError); found {
+				err = fmt.Errorf("ip -batch: %s",
+					string(ee.Stderr))
+				fmt.Println("wait err", err)
+			}
+		}()
 		for _, dev := range interfaces {
-			if strings.HasPrefix(dev.Name, "eth-") ||
-				strings.HasPrefix(dev.Name, "ixge") ||
-				strings.HasPrefix(dev.Name, "meth-") {
-				exec.Command("/bin/ip", "link", "delete",
-					dev.Name).Run()
+			if strings.HasPrefix(dev.Name, "eth-") {
+				_, err = fmt.Fprintln(w, "link", "delete",
+					dev.Name)
+				if err != nil {
+					fmt.Println("write err", err)
+					return err
+				}
 			}
 		}
 		return nil
