9fa4cd10f5666ef3a542c887ad3fc09fd6968ad5
Eliot Dresselhaus <edressel@platinasystems.com>
2017-08-15 09:10:49 +0200

    go vet
    Signed-off-by: Eliot Dresselhaus <edressel@platinasystems.com>

--- vnet/ip4/package.go
+++ vnet/ip4/package.go
@@ -80,7 +80,7 @@ func (m *Main) ParseLayer(b []byte, in *parse.Input) (n uint) {
 		if l, ok := m.GetLayer(h.Protocol); ok {
 			n += l.ParseLayer(b[n:], in)
 		} else {
-			panic(fmt.Errorf("no parser for protocol %s", h.Protocol))
+			panic(fmt.Errorf("no parser for protocol %v", h.Protocol))
 		}
 	}
 	return
--- vnet/ip6/package.go
+++ vnet/ip6/package.go
@@ -56,7 +56,7 @@ func (m *Main) FormatLayer(b []byte) (lines []string) {
 		if l, ok := m.GetLayer(h.Protocol); ok {
 			lines = append(lines, l.FormatLayer(b[n:])...)
 		} else {
-			panic(fmt.Errorf("no formatter for protocol %s", h.Protocol))
+			panic(fmt.Errorf("no formatter for protocol %v", h.Protocol))
 		}
 	}
 	return
@@ -70,7 +70,7 @@ func (m *Main) ParseLayer(b []byte, in *parse.Input) (n uint) {
 		if l, ok := m.GetLayer(h.Protocol); ok {
 			n += l.ParseLayer(b[n:], in)
 		} else {
-			panic(fmt.Errorf("no parser for protocol %s", h.Protocol))
+			panic(fmt.Errorf("no parser for protocol %v", h.Protocol))
 		}
 	}
 	return
