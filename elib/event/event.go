d4705e1f24f047014fbf0bda3e4721af83af399d
Eliot Dresselhaus <edressel@platinasystems.com>
2017-08-15 22:08:17 +0200

    Signed-off-by: Eliot Dresselhaus <edressel@platinasystems.com>

--- elib/event/event.go
+++ elib/event/event.go
@@ -7,6 +7,8 @@ package event
 import (
 	"github.com/platinasystems/go/elib"
 	"github.com/platinasystems/go/elib/cpu"
+
+	"sync"
 )
 
 type Actor interface {
@@ -29,26 +31,30 @@ func (p *timedEventPool) Compare(i, j int) int {
 //go:generate gentemplate -d Package=event -id timedEvent -d PoolType=timedEventPool -d Type=TimedActor -d Data=events github.com/platinasystems/go/elib/pool.tmpl
 
 type Pool struct {
+	mu      sync.Mutex
 	pool    timedEventPool
 	fibheap elib.FibHeap
 }
 
-func (p *Pool) Elts() uint                   { return p.pool.Elts() }
-func (p *Pool) Foreach(f func(e TimedActor)) { p.pool.Foreach(f) }
+func (p *Pool) Elts() uint { return p.pool.Elts() }
 
 func (p *Pool) Add(e TimedActor) (ei uint) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
 	ei = p.pool.GetIndex()
 	p.pool.events[ei] = e
 	p.fibheap.Add(ei)
 	return ei
 }
 
-func (p *Pool) Del(ei uint) {
+func (p *Pool) del(ei uint) {
 	p.fibheap.Del(ei)
 	p.pool.PutIndex(ei)
 }
 
 func (p *Pool) advance(t cpu.Time, iv *ActorVec) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
 	for {
 		ei, valid := p.fibheap.Min(&p.pool)
 		if !valid {
@@ -58,7 +64,7 @@ func (p *Pool) advance(t cpu.Time, iv *ActorVec) {
 		if e.EventTime() > t {
 			break
 		}
-		p.fibheap.Del(ei)
+		p.del(ei)
 		if iv != nil {
 			*iv = append(*iv, e)
 		} else {
@@ -68,6 +74,8 @@ func (p *Pool) advance(t cpu.Time, iv *ActorVec) {
 }
 
 func (p *Pool) NextTime() (t cpu.Time, valid bool) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
 	ei, valid := p.fibheap.Min(&p.pool)
 	if valid {
 		t = p.pool.events[ei].EventTime()
