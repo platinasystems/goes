1959c9c3b15e2ca3b610b70a983440ac02a9a39b
Eliot Dresselhaus <edressel@platinasystems.com>
2017-08-19 12:04:15 +0200

    caller pc
    Signed-off-by: Eliot Dresselhaus <edressel@platinasystems.com>

--- elib/cpu/cpu_amd64.s
+++ elib/cpu/cpu_amd64.s
@@ -5,3 +5,9 @@ TEXT ·TimeNow(SB),$0-0
 	ADDQ DX, AX
         MOVQ AX, ret+0(FP)
         RET
+
+TEXT ·GetCallerPC(SB),4,$8-16
+	MOVQ	argp+0(FP),AX		// addr of first arg
+	MOVQ	-8(AX),AX		// get calling pc
+	MOVQ	AX, ret+8(FP)
+	RET
