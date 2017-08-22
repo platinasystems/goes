fb632302d9b0f6143d0e479b96586415bec0200c
Eliot Dresselhaus <edressel@platinasystems.com>
2017-08-22 12:59:21 +0200

    Signed-off-by: Eliot Dresselhaus <edressel@platinasystems.com>

--- elib/elog/elogview/viewer.go
+++ elib/elog/elogview/viewer.go
@@ -10,6 +10,7 @@ import (
 
 	"fmt"
 	"math"
+	"regexp"
 	"strings"
 	"sync"
 )
@@ -20,34 +21,84 @@ func (c *ctx) cr() *cairo.Context { return (*cairo.Context)(c) }
 func (c *ctx) translate(x r2.V)   { c.cr().Translate(x.X(), x.Y()) }
 func (c *ctx) scale(x r2.V)       { c.cr().Scale(x.X(), x.Y()) }
 
-type rgba struct{ r, g, b, a float64 }
+type rgb struct{ r, g, b float64 }
+type rgba struct {
+	rgb
+	a float64
+}
 
-func (x rgba) RGBA() (r, g, b, a float64) { r, g, b, a = x.r, x.g, x.b, x.a; return }
-func gray(x, a float64) rgba              { return rgba{r: x, g: x, b: x, a: a} }
+func Rgb(r, g, b float64) rgb    { return rgb{r: r, g: g, b: b} }
+func Rgba(r rgb, a float64) rgba { return rgba{rgb: r, a: a} }
 
-func (x rgba) darken(f float64) (y rgba) {
-	y.r = f * x.r
-	y.g = f * x.g
-	y.b = f * x.b
-	y.a = x.a
+func (x *rgb) RGB() (r, g, b float64)      { r, g, b = x.r, x.g, x.b; return }
+func (x *rgba) RGBA() (r, g, b, a float64) { r, g, b, a = x.r, x.g, x.b, x.a; return }
+
+func gray(f float64) rgb { return rgb{r: f, g: f, b: f} }
+func (x *rgb) gray(f float64) {
+	x.r = 1 - f
+	x.g = 1 - f
+	x.b = 1 - f
+}
+
+func grayWheel(f0 float64, i, n int) (r rgb) {
+	dx := (.5 - f0) / float64(n/2)
+	r.gray(f0 + float64(i)*dx)
 	return
 }
 
-func (x rgba) lighten(f float64) (y rgba) {
-	y.r = 1 - f*(1-x.r)
-	y.g = 1 - f*(1-x.g)
-	y.b = 1 - f*(1-x.b)
-	y.a = x.a
+// Mix primary r0 with a bit of secondary r1 (specified by fraction f1 in [0,1]).
+func (r0 rgb) mix(r1 rgb, f1 float64) (r rgb) {
+	r.r = f1*r1.r + (1-f1)*r0.r
+	r.g = f1*r1.g + (1-f1)*r0.g
+	r.b = f1*r1.b + (1-f1)*r0.b
 	return
 }
 
-var standardColors = [...]rgba{
-	rgba{r: 1},              // red
-	rgba{g: 1, a: .3},       // green
-	rgba{b: 1, a: .3},       // blue
-	rgba{r: 1, b: 1, a: .3}, // yellow
-	rgba{g: 1, b: 1, a: .3},
-	rgba{r: 1, b: 1},
+var (
+	black = rgb{r: 0, g: 0, b: 0}
+	white = rgb{r: 1, g: 1, b: 1}
+)
+
+func (x rgb) darken(f float64) rgb  { return x.mix(black, f) }
+func (x rgb) lighten(f float64) rgb { return x.mix(white, f) }
+
+var standardColors = [...]decoration{
+	decoration{ // dark blue
+		bg: Rgb(.05, .24, .33),
+	},
+	decoration{ // dark blue
+		bg: Rgb(.06, .36, .47),
+	},
+	decoration{ // light blue
+		bg: Rgb(.07, .47, .60),
+	},
+	decoration{ // light blue
+		bg: Rgb(.07, .58, .73),
+	},
+	decoration{ // teal
+		bg: Rgb(.36, .65, .58),
+	},
+	decoration{
+		bg: Rgb(.64, .72, .42),
+	},
+	decoration{
+		bg: Rgb(.92, .78, .27),
+	},
+	decoration{ // yellow
+		bg: Rgb(.93, .67, .22),
+	},
+	decoration{ // orange
+		bg: Rgb(.94, .55, .17),
+	},
+	decoration{ // orange
+		bg: Rgb(.95, .42, .13),
+	},
+	decoration{ // red
+		bg: Rgb(.85, .31, .12),
+	},
+	decoration{ // red
+		bg: Rgb(.75, .18, .11),
+	},
 }
 
 const (
@@ -68,8 +119,8 @@ func (c *ctx) text_box(lines []text_line) (bounding_box r2.V) {
 	for i := range lines {
 		te := cr.TextExtents(lines[i].s)
 		lines[i].e = te
-		if te.Width > max_width {
-			max_width = te.Width
+		if w := te.Width; w > max_width {
+			max_width = w
 		}
 	}
 	bounding_box = r2.XY(max_width, fe.Height*float64(len(lines)))
@@ -122,6 +173,7 @@ func (x *ctx) roundedRect(x0, dx r2.V, r float64) {
 
 type viewer struct {
 	win              *gtk.Window
+	box              *gtk.Box
 	screen_dpi       float64
 	da               *gtk.DrawingArea
 	eb               *gtk.EventBox
@@ -129,8 +181,10 @@ type viewer struct {
 	ev               *elog.View
 	ves              []visible_event
 	selected_events  map[uint]struct{}
+	hidden_callers   map[uint]struct{}
 	m                map[uint64]*decoration
 	pointer          pointer_state
+	filters
 	Config
 }
 
@@ -160,16 +214,21 @@ func View(ev *elog.View, cf Config) {
 	v.eb.SetCanFocus(true)
 	v.eb.SetEvents(int(gdk.KEY_PRESS_MASK |
 		gdk.BUTTON_PRESS_MASK | gdk.BUTTON_RELEASE_MASK | gdk.POINTER_MOTION_MASK))
-	v.win.Add(v.eb)
 
 	v.da, _ = gtk.DrawingAreaNew()
 	v.eb.Add(v.da)
+
+	v.box, _ = gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 0)
+	v.box.PackStart(v.eb, true, true, 0)
+	v.win.Add(v.box)
 
 	v.da.Connect("draw", v.draw_events)
 	v.eb.Connect("button_press_event", v.button_press)
 	v.eb.Connect("button_release_event", v.button_release)
 	v.eb.Connect("motion_notify_event", v.pointer_motion)
 	v.eb.Connect("key_press_event", v.key_press)
+
+	v.filters.init(v)
 
 	v.win.ShowAll()
 	gtk.Main()
@@ -186,10 +245,25 @@ func (v *viewer) key_press(eb *gtk.EventBox, ev *gdk.Event) {
 	t_min, t_max := t.Min, t.Max
 	switch key {
 	case gdk.KEY_Escape:
+		for ci, _ := range v.hidden_callers {
+			delete(v.hidden_callers, ci)
+		}
+		for ei, _ := range v.selected_events {
+			delete(v.selected_events, ei)
+		}
 		break
 	case gdk.KEY_q:
 		if v.EnableKeyboardQuit { // quit does not work via key inside vnet
 			v.quit()
+		}
+	case gdk.KEY_BackSpace:
+		for ei, _ := range v.selected_events {
+			e := &v.ev.Events[ei]
+			if v.hidden_callers == nil {
+				v.hidden_callers = make(map[uint]struct{})
+			}
+			v.hidden_callers[e.GetCaller()] = struct{}{}
+			delete(v.selected_events, ei)
 		}
 	case gdk.KEY_r:
 		v.ev.Reset()
@@ -339,7 +413,7 @@ func (v *viewer) draw_selected_event(cr *cairo.Context, e *elog.Event, ve *visib
 	}
 
 	ec := ev.EventCaller(e)
-	d, _ := v.decorationForPc(ec.PC)
+	d, _ := v.decorationForCaller(ec)
 
 	sf, _ := ec.ShortPath(ec.File, 32)
 	sn, _ := ec.ShortPath(ec.Name, 32)
@@ -354,18 +428,21 @@ func (v *viewer) draw_selected_event(cr *cairo.Context, e *elog.Event, ve *visib
 	}
 
 	c := (*ctx)(cr)
-	cr.SetFontSize(10)
+	cr.SetFontSize(12)
 	bbox := c.text_box(ve.l)
 
 	center := ve.rr.Center
-	const radius = 4
-	rr_dx := bbox + r2.XY(2*radius, radius)
-	c.roundedRect(center-rr_dx/2, rr_dx, radius)
-	dc := d.color.lighten(.375)
-	cr.SetSourceRGBA(dc.r, dc.g, dc.b, 1)
+	const radius = 8
+	rr_dx := bbox + 2*r2.XY(radius, radius)
+	upper_left := center - rr_dx/2
+	c.roundedRect(upper_left, rr_dx, radius)
+	bg := d.bg.lighten(.75)
+	c.set_pattern(upper_left, rr_dx, bg, .15)
 	cr.Fill()
 
-	cr.SetSourceRGBA(0, 0, 0, 1)
+	fg := grayWheel(.06, 8, 9)
+	fg = fg.mix(d.bg, .25)
+	cr.SetSourceRGB(fg.RGB())
 	c.text(center-r2.XY(bbox.X()/2, 0), text_align_left, ve.l...)
 	cr.Stroke()
 
@@ -373,20 +450,26 @@ func (v *viewer) draw_selected_event(cr *cairo.Context, e *elog.Event, ve *visib
 }
 
 type decoration struct {
-	color rgba
+	bg rgb
 }
 
-func (v *viewer) decorationForPc(pc uint64) (d *decoration, ok bool) {
+func (c *ctx) set_pattern(x, dx r2.V, bg rgb, f float64) {
+	p, _ := cairo.NewPatternLinear(x.X(), x.Y(), x.X(), x.Y()+dx.Y())
+	l, d := bg.lighten(f), bg.darken(f)
+	p.AddColorStopRGBA(0, l.r, l.g, l.b, 1)
+	p.AddColorStopRGBA(1, d.r, d.g, d.b, 1)
+	c.cr().SetSource(p)
+}
+
+func (v *viewer) decorationForCaller(ci *elog.CallerInfo) (d *decoration, ok bool) {
+	pc := ci.PC
 	if v.m == nil {
 		v.m = make(map[uint64]*decoration)
 	}
 	if d, ok = v.m[pc]; !ok {
 		i := len(v.m)
 		i0 := i % len(standardColors)
-		d = &decoration{color: standardColors[i0]}
-		if d.color.a == 0 {
-			d.color.a = .2
-		}
+		d = &standardColors[i0]
 		v.m[pc] = d
 	}
 	return
@@ -441,20 +524,20 @@ func (v *viewer) x_to_time(x float64) (t float64) {
 	return
 }
 
-func (c *ctx) time_axis(v *viewer, t float64, dx r2.V, text_align int, bg rgba, line bool, format string, args ...interface{}) {
+func (c *ctx) time_axis(v *viewer, t float64, dx r2.V, text_align int, bg rgb, line bool, format string, args ...interface{}) {
 	dw := v.eb_border
 	w := v.eb_dx - 2*dw
 	x := r2.XY(v.time_to_x(t), dw.Y())
 	s := fmt.Sprintf(format, args...)
 
-	c.cr().SetSourceRGBA(0, 0, 0, 1)
+	c.cr().SetSourceRGB(0, 0, 0)
 	c.cr().SetFontSize(10)
 	c.textf(x+dx, text_align, s)
 	c.cr().Stroke()
 
 	if line {
-		color := bg.darken(.85)
-		c.cr().SetSourceRGBA(color.RGBA())
+		color := bg.darken(.15)
+		c.cr().SetSourceRGB(color.RGB())
 		c.moveTo(x)
 		c.lineTo(x + r2.XY(0, w.Y()))
 		c.cr().Stroke()
@@ -471,41 +554,16 @@ func (v *viewer) draw_events(da *gtk.DrawingArea, cr *cairo.Context) {
 	t_min, t_max := tb.Min, tb.Max
 
 	c := (*ctx)(cr)
+	cr.SelectFontFace("Mono", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
 	cr.SetAntialias(cairo.ANTIALIAS_SUBPIXEL)
 
 	// Background
-	bg_color := gray(.9, 1)
-	c.rect(dw, w)
-	cr.SetSourceRGBA(bg_color.RGBA())
-	cr.Fill()
+	bg_color := grayWheel(.06, 0, 9)
+	draw_axis_line := true
 
 	// Axes
-	c.time_axis(v, t_min, r2.XY(0, -10), text_align_center, bg_color, false, "%.0f%s", t_min/tb.Unit, tb.UnitName)
-	c.time_axis(v, t_max, r2.XY(0, -10), text_align_center, bg_color, false, "%.0f%s", t_max/tb.Unit, tb.UnitName)
-
-	// Pointer bounds.
-	{
-		p := &v.pointer
-		if l := len(p.xs); p.val != 0 && l >= 4 {
-			x0, x1 := p.xs[0].X(), p.xs[l-1].X()
-			if x0 > x1 {
-				x1, x0 = x0, x1
-			}
-			hilight_color := bg_color.darken(.9)
-			cr.SetSourceRGBA(hilight_color.RGBA())
-			c.rect(r2.XY(x0, dw.Y()), r2.XY(x1-x0, w.Y()))
-			cr.Fill()
-			t0, t1 := v.x_to_time(x0), v.x_to_time(x1)
-			const dy = 10
-			c.time_axis(v, t0, r2.XY(-dy/2, dy), text_align_right, hilight_color, true, "%.2f", t0/tb.Unit)
-			c.time_axis(v, t1, r2.XY(+dy/2, dy), text_align_left, hilight_color, true, "%.2f", t1/tb.Unit)
-			if x1-x0 > 20 {
-				c.cr().SetSourceRGBA(0, 0, 0, 1)
-				c.textf(r2.XY(.5*(x0+x1), dw.Y()+dy), text_align_center, "%.2f%s", (t1-t0)/tb.Unit, tb.UnitName)
-				cr.Stroke()
-			}
-		}
-	}
+	c.time_axis(v, t_min, r2.XY(0, -10), text_align_center, bg_color, draw_axis_line, "%.0f%s", t_min/tb.Unit, tb.UnitName)
+	c.time_axis(v, t_max, r2.XY(0, -10), text_align_center, bg_color, draw_axis_line, "%.0f%s", t_max/tb.Unit, tb.UnitName)
 
 	// Title
 	{
@@ -516,13 +574,12 @@ func (v *viewer) draw_events(da *gtk.DrawingArea, cr *cairo.Context) {
 		cr.Fill()
 	}
 
-	var x_last elib.Float64Vec
-
 	if v.ves != nil {
 		v.ves = v.ves[:0]
 	}
 
 	// Draw events.
+	var x_last elib.Float64Vec
 	for i := range ev.Events {
 		e := &ev.Events[i]
 		t := ev.ElapsedTime(e)
@@ -533,85 +590,104 @@ func (v *viewer) draw_events(da *gtk.DrawingArea, cr *cairo.Context) {
 			continue
 		}
 
+		if _, ok := v.hidden_callers[e.GetCaller()]; ok {
+			continue
+		}
+
 		lines := e.Strings(ev.GetContext())
 		ci := ev.EventCaller(e)
-		pc := ci.PC
-		d, _ := v.decorationForPc(pc)
+		d, _ := v.decorationForCaller(ci)
 
 		center := dw + x
 		radius := 4.
-		cr.SetSourceRGBA(d.color.r, d.color.g, d.color.b, d.color.a)
-		cr.SetFontSize(9)
+		cr.SetSourceRGB(d.bg.RGB())
+		cr.SetFontSize(10)
 		fe, te := cr.FontExtents(), cr.TextExtents(lines[0])
 
+		var tl [1]text_line
+		ve.l = tl[:]
+		ve.l[0].s = lines[0]
+		bbox := c.text_box(ve.l)
+		ve.rr.Size = bbox + r2.XY(4*radius, radius)
+
 		// Choose integer Y such that text will not overlap with other text.
-		iy := uint(0)
-		const iy_max = 64
-		for iy < iy_max {
-			x_last.ValidateInit(uint(iy), -1e10)
-			if x_left := center.X() - te.Width/2; x_last[iy]+2*radius < x_left {
-				x_last[iy] = center.X() + te.Width/2
+		var (
+			idy     int
+			visible bool
+		)
+		y_level := uint(0)
+		for {
+			// Avoid level 0 which will be used to plot event points.  See (A) below.
+			idy = int(y_level + 1)
+			// Odd levels go above baseline; even go below.
+			if idy%2 != 0 {
+				idy = (idy + 1) / 2
+			} else {
+				idy = -idy / 2
+			}
+
+			// Rounded rect with event text inside.
+			ve.rr.Center = center - r2.XY(0, (fe.Height+2*radius)*float64(idy))
+			if visible = ve.rr.Center.Y() >= dw.Y() && ve.rr.Center.Y()+ve.rr.Size.Y() <= w.Y()+dw.Y(); !visible {
 				break
 			}
-			iy++
+
+			x_last.ValidateInit(uint(y_level), -1e10)
+			if x_left := center.X() - te.Width/2; x_last[y_level]+2*radius < x_left {
+				x_last[y_level] = center.X() + te.Width/2
+				break
+			}
+			y_level++
 		}
-		if visible := iy <= iy_max; !visible {
+		if !visible {
 			continue
 		}
 
 		ve.ei = uint(i)
 
-		// Avoid level 0 which will be used to plot event points.  See (A) below.
-		iy++
-
-		// Odd goes above baseline; even goes below baseline.
-		var idy int
-		if iy%2 != 0 {
-			idy = int((iy + 1) / 2)
-		} else {
-			idy = -int(iy / 2)
-		}
-
-		// Rounded rect with event text inside.
-		ve.rr.Center = center - r2.XY(0, (fe.Height+2*radius)*float64(idy))
-
-		if visible := ve.rr.Center.Y() >= dw.Y() && ve.rr.Center.Y()+dw.Y() < w.Y(); !visible {
-			continue
-		}
-
-		cr.SetSourceRGB(0, 0, 0)
-		tw := c.textf(ve.rr.Center, text_align_center, lines[0])
-		ve.rr.Size = tw + r2.XY(2*radius, radius)
-		cr.Stroke()
-		cr.SetSourceRGBA(d.color.r, d.color.g, d.color.b, d.color.a)
-		c.roundedRect(ve.rr.Center-ve.rr.Size/2, ve.rr.Size, radius)
+		upper_left := ve.rr.Center - ve.rr.Size/2
+		bg := d.bg.lighten(.5)
+		fg := black
+		f := .1
+		c.set_pattern(upper_left, ve.rr.Size, bg, f)
+		c.roundedRect(upper_left, ve.rr.Size, radius)
 		cr.Fill()
+		cr.SetSourceRGB(fg.RGB())
+		c.textf(ve.rr.Center, text_align_center, lines[0])
+		cr.Stroke()
 
 		// (A) Plot event points on event line at iy == 0 and lines between text & event line.
-		max_idy := 4
-		if idy >= -max_idy && idy <= +max_idy {
+		{
+			cr.Save()
+			const max_idy = 6
+			show_line := idy >= -max_idy && idy <= +max_idy
 			dot_radius := .9 * radius
-			if idy > 0 {
-				c.moveTo(ve.rr.Center + r2.XY(0, (ve.rr.Size/2).Y()))
-				c.lineTo(center - r2.XY(0, dot_radius))
-			} else {
-				c.moveTo(ve.rr.Center - r2.XY(0, (ve.rr.Size/2).Y()))
-				c.lineTo(center + r2.XY(0, dot_radius))
+			if show_line {
+				if idy > 0 {
+					c.moveTo(ve.rr.Center + r2.XY(0, (ve.rr.Size/2).Y()))
+					c.lineTo(center - r2.XY(0, dot_radius))
+				} else {
+					c.moveTo(ve.rr.Center - r2.XY(0, (ve.rr.Size/2).Y()))
+					c.lineTo(center + r2.XY(0, dot_radius))
+				}
+				cr.SetOperator(cairo.OPERATOR_OVER)
+				cr.SetSourceRGBA(d.bg.r, d.bg.g, d.bg.b, .5)
+				cr.SetLineWidth(2)
+				cr.Stroke()
 			}
-			cr.SetOperator(cairo.OPERATOR_ATOP)
-			cr.SetLineWidth(2)
-			cr.Stroke()
 
-			cr.SetSourceRGBA(d.color.r, d.color.g, d.color.b, .75)
 			cr.MoveTo(center.X(), center.Y())
 			cr.Arc(center.X(), center.Y(), dot_radius, 0, 2*math.Pi)
+			cr.SetSourceRGBA(d.bg.r, d.bg.g, d.bg.b, 1)
 			cr.Fill()
+			cr.Restore()
 		}
 
 		// Save away for later use.
 		v.ves = append(v.ves, ve)
 	}
 
+	// Selected events.
 	for i := range v.ves {
 		ve := &v.ves[i]
 		if v.is_selected(ve.ei) {
@@ -619,4 +695,134 @@ func (v *viewer) draw_events(da *gtk.DrawingArea, cr *cairo.Context) {
 			v.draw_selected_event(cr, e, ve)
 		}
 	}
+
+	// Pointer bounds.
+	{
+		p := &v.pointer
+		if l := len(p.xs); p.val != 0 && l >= 4 {
+			x0, x1 := p.xs[0].X(), p.xs[l-1].X()
+			if x0 > x1 {
+				x1, x0 = x0, x1
+			}
+			bg, fg := grayWheel(.06, 4, 9), grayWheel(.06, 2, 9)
+			cr.SetSourceRGBA(bg.r, bg.g, bg.b, .25)
+			c.rect(r2.XY(x0, dw.Y()), r2.XY(x1-x0, w.Y()))
+			cr.Fill()
+			t0, t1 := v.x_to_time(x0), v.x_to_time(x1)
+			const dy = 10
+			c.time_axis(v, t0, r2.XY(-dy/2, dy), text_align_right, fg, true, "%.1f%s", t0/tb.Unit, tb.UnitName)
+			c.time_axis(v, t1, r2.XY(+dy/2, dy), text_align_left, fg, true, "%.1f%s", t1/tb.Unit, tb.UnitName)
+			if x1-x0 > 30 {
+				c.cr().SetSourceRGBA(0, 0, 0, 1)
+				c.textf(r2.XY(.5*(x0+x1), dw.Y()+dy), text_align_center, "%.2f%s", (t1-t0)/tb.Unit, tb.UnitName)
+				cr.Stroke()
+			}
+		}
+	}
+}
+
+type filters struct {
+	v              *viewer
+	hbox           *gtk.Box
+	add_button     *gtk.Button
+	entry          *gtk.Entry
+	filter_by_name map[string]*filter
+}
+
+type filter struct {
+	m            *filters
+	name         string
+	check_button *gtk.CheckButton
+	re           *regexp.Regexp
+}
+
+func (f *filter) matchEvent(e *elog.Event) (ok bool) {
+	ev := f.m.v.ev
+	ec := ev.EventCaller(e)
+	lines := e.Strings(ev.GetContext())
+	if ok = f.re.MatchString(lines[0]); ok {
+		return
+	}
+	if ok = f.re.MatchString(ec.Name); ok {
+		return
+	}
+	return
+}
+func (f *filter) nMatching() (n uint) {
+	v := f.m.v
+	for i := range v.ev.Events {
+		e := &v.ev.Events[i]
+		if f.matchEvent(e) {
+			n++
+		}
+	}
+	return
+}
+
+func (f *filter) check_button_clicked(b *gtk.CheckButton, e *gdk.Event) {
+	be := gdk.EventButton{e}
+	if be.ButtonVal() == 3 {
+		f.check_button.Destroy()
+		delete(f.m.filter_by_name, f.name)
+		f.m.hbox.ShowAll()
+		f.m.v.eb.QueueDraw()
+		return
+	}
+}
+
+func (m *filters) button_clicked(b *gtk.Button, e *gdk.Event) {
+	buf, _ := m.entry.GetBuffer()
+
+	name, _ := buf.GetText()
+	name = strings.TrimSpace(name)
+	if len(name) == 0 {
+		return
+	}
+
+	if _, ok := m.filter_by_name[name]; ok {
+		return
+	}
+
+	f := &filter{m: m, name: name}
+
+	var err error
+	f.re, err = regexp.Compile(name)
+
+	label := f.name
+	if err != nil {
+		label += " (invalid: " + err.Error() + ")"
+	} else {
+		label += fmt.Sprintf(" (%d)", f.nMatching())
+	}
+	cb, _ := gtk.CheckButtonNewWithLabel(label)
+	cb.SetActive(err == nil)
+	m.hbox.PackStart(cb, false, false, 10)
+
+	if m.filter_by_name == nil {
+		m.filter_by_name = make(map[string]*filter)
+	}
+	m.filter_by_name[f.name] = f
+
+	f.check_button = cb
+	cb.Connect("button_press_event", f.check_button_clicked)
+	m.hbox.ShowAll()
+	m.v.eb.QueueDraw()
+}
+
+func (m *filters) init(v *viewer) {
+	m.v = v
+	m.hbox, _ = gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 0)
+
+	b, _ := gtk.ButtonNewWithLabel("Add Filter")
+	b.SetSizeRequest(30, 30)
+	m.add_button = b
+	m.hbox.PackStart(b, false, false, 0)
+	b.Connect("button_press_event", m.button_clicked)
+
+	e, _ := gtk.EntryNew()
+	e.SetSizeRequest(50, 30)
+	m.entry = e
+	m.hbox.PackStart(e, false, false, 10)
+
+	v.box.PackStart(m.hbox, false, false, 0)
 }
