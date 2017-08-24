e50a42db917730d64d35f266daf073b715ba443f
Eliot Dresselhaus <edressel@platinasystems.com>
2017-08-24 12:49:09 +0200

    Signed-off-by: Eliot Dresselhaus <edressel@platinasystems.com>

---
+++ elib/decode_address.go
@@ -0,0 +1,45 @@
+// Copyright 2016 Platina Systems, Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package elib
+
+import (
+	"fmt"
+	"reflect"
+	"sort"
+)
+
+func DecodeAddress(x interface{}, address uint) (path []string, t reflect.Type) {
+	t = reflect.ValueOf(x).Type()
+	if t.Kind() == reflect.Ptr {
+		t = t.Elem()
+	}
+	addr := uintptr(address)
+	for {
+		switch t.Kind() {
+		case reflect.Struct:
+			nf := t.NumField()
+			fi := sort.Search(nf, func(i int) bool {
+				f := t.Field(i)
+				lo, hi := f.Offset, f.Offset+f.Type.Size()
+				if found := addr >= lo && addr < hi; found {
+					path = append(path, f.Name)
+					t = f.Type
+					addr -= lo
+				}
+				return addr < lo
+			})
+			if fi >= nf {
+				panic(fmt.Errorf("not found %s 0x%x", t.Name(), addr))
+			}
+		case reflect.Array:
+			t = t.Elem()
+			i0, i1 := addr/t.Size(), addr%t.Size()
+			path = append(path, fmt.Sprintf("[%d]", i0))
+			addr = i1
+		default:
+			return
+		}
+	}
+}
