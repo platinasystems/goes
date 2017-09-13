46d9dffc074bfc8957747c25ad03e0f6e93ce796
Eliot Dresselhaus <edressel@platinasystems.com>
2017-09-13 15:08:37 +0200

    unix-rx chan blocking
    Signed-off-by: Eliot Dresselhaus <edressel@platinasystems.com>

--- vnet/unix/rx.go
+++ vnet/unix/rx.go
@@ -106,9 +106,9 @@ func (n *rx_node) init(m *Main) {
 	v.RegisterInputNode(n, "unix-rx")
 	n.buffer_pool = vnet.DefaultBufferPool
 	v.AddBufferPool(n.buffer_pool)
-	n.pv_pool = make(chan *rx_packet_vector, 2*vnet.MaxVectorLen)
-	n.rv_pool = make(chan *rx_ref_vector, 2*vnet.MaxVectorLen)
-	n.rv_input = make(chan *rx_ref_vector, 2*vnet.MaxVectorLen)
+	n.pv_pool = make(chan *rx_packet_vector, 4*vnet.MaxVectorLen)
+	n.rv_pool = make(chan *rx_ref_vector, 4*vnet.MaxVectorLen)
+	n.rv_input = make(chan *rx_ref_vector, 4*vnet.MaxVectorLen)
 	n.max_buffers_per_packet = max_rx_packet_size / n.buffer_pool.Size
 	if max_rx_packet_size%n.buffer_pool.Size != 0 {
 		n.max_buffers_per_packet++
