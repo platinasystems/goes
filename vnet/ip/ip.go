8b4172a1b9923c3c3dd70c0153f68ebe2d878005
Eliot Dresselhaus <edressel@platinasystems.com>
2017-09-20 10:11:58 +0200

    normalize naming
    Signed-off-by: Eliot Dresselhaus <edressel@platinasystems.com>

--- vnet/ip/ip.go
+++ vnet/ip/ip.go
@@ -21,6 +21,11 @@ const (
 // Generic ip4/ip6 address: big enough for either.
 type Address [16]uint8
 
+// Address 32-bit chunk in network byte order.
+func (a *Address) AsUint32(i uint) vnet.Uint32 {
+	return vnet.Uint32(a[4*i+3]) | vnet.Uint32(a[4*i+2])<<8 | vnet.Uint32(a[4*i+1])<<16 | vnet.Uint32(a[4*i+0])<<24
+}
+
 func (a *Address) Add(x uint64)          { vnet.ByteAdd(a[:], x) }
 func AddressUint64(x uint64) (a Address) { a.Add(x); return }
 func AddressUint32(x uint32) (a Address) { vnet.ByteAdd(a[:4], uint64(x)); return }
