b81c7933eb2268597bbcf55b45e172d09b5a4377
Aurelio Rios <aurelio@platinasystems.com>
2017-10-17 15:39:57 -0700

    -Added suport for CH1-Mc to diagHost, diagI2c, diagPower, and diagProm. -Corrected PEN offset on VendorExtension in eeprom. -comment-out modules not used on CH1-Mc in goes
    Signed-off-by: Aurelio Rios <aurelio@platinasystems.com>

--- goes/cmd/platina/mk1/bmc/diag/diagHost.go
+++ goes/cmd/platina/mk1/bmc/diag/diagHost.go
@@ -7,12 +7,23 @@ package diag
 import (
 	"fmt"
 	"time"
+
+	"github.com/platinasystems/go/internal/eeprom"
 )
 
 func diagHost() error {
+        const (
+                TOR1 uint8      = 0x00
+                CH1 uint8       = 0x01
+                CH1MC uint8     = 0x04
+                CH1LC uint8     = 0x05
+        )
 
 	var r string
 	var d uint8
+
+	// Common code between TOR1, CH1MC, and CH1LC
+	//
 	fmt.Printf("\n%15s|%25s|%10s|%10s|%10s|%10s|%6s|%35s\n", "function", "parameter", "units", "value", "min", "max", "result", "description")
 	fmt.Printf("---------------|-------------------------|----------|----------|----------|----------|------|-----------------------------------\n")
 
@@ -94,6 +105,32 @@ func diagHost() error {
 	fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "host", "bmc_to_host_rst_l_off", "-", pinstate, active_low_off_min, active_low_off_max, r, "rst off, check rst=1 on cpu card")
 	gpioSet("CPU_TO_MAIN_I2C_EN", false)
 
+
+
+        dev := eeprom.Device{
+                BusIndex:   0,
+                BusAddress: 0x55,
+        }
+        if err:= dev.GetInfo(); err != nil {
+                return err
+        }
+        if dev.Fields.ChassisType == TOR1 {
+                if err := diagHostTor(); err != nil {
+                        return err
+                }
+        } else if (dev.Fields.ChassisType == CH1) && (dev.Fields.BoardType == CH1MC) {
+                return nil
+        } else if (dev.Fields.ChassisType == CH1) && (dev.Fields.BoardType == CH1MC) {
+                diagHostCh1Lc()
+        }
+
+        return nil
+}
+
+
+func diagHostTor() error {
+        var r string
+
 	/* diagTest: HOST_TO_BMC_I2C_GPIO
 	toggle HOST_TO_BMC_I2C_GPIO and validate bmc can detect the proper signal states
 	*/
@@ -101,7 +138,7 @@ func diagHost() error {
 	time.Sleep(50 * time.Millisecond)
 	diagI2cWriteOffsetByte(0x00, 0x74, 0x02, 0xF7)
 	diagI2cWriteOffsetByte(0x00, 0x74, 0x06, 0xD7)
-	pinstate, err = gpioGet("HOST_TO_BMC_I2C_GPIO")
+	pinstate, err := gpioGet("HOST_TO_BMC_I2C_GPIO")
 	if err != nil {
 		return err
 	}
@@ -124,5 +161,12 @@ func diagHost() error {
 	}
 	fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "host", "host_to_bmc_i2c_gpio_high", "-", pinstate, active_high_on_min, active_high_on_max, r, "check gpio is low")
 	gpioSet("CPU_TO_MAIN_I2C_EN", false)
+
+	return nil
+}
+
+
+func diagHostCh1Lc() error {
+
 	return nil
 }
--- goes/cmd/platina/mk1/bmc/diag/diagI2c.go
+++ goes/cmd/platina/mk1/bmc/diag/diagI2c.go
@@ -21,8 +21,18 @@ var s [MAXOPS]R
 var i = I{false, i2c.RW(0), 0, 0, b, 0, 0, 0}
 var x int
 var b = [34]byte{0}
+var chassisType, boardType uint8
 
-const MAXOPS = 30
+const (
+	MAXOPS = 30
+
+	TOR1 uint8	= 0x00
+	CH1 uint8 	= 0x01
+
+	CH1MC uint8	= 0x04
+	CH1LC uint8	= 0x05
+)
+
 
 var sd i2c.SMBusData
 
@@ -41,28 +51,43 @@ type R struct {
 	E error
 }
 
+var ucd9090dAdr uint8
+var ledgpiodAdr uint8
+
 func diagI2c() error {
+	ucd9090dAdr = 0x34
+	ledgpiodAdr = 0x75
 
-	var ucd9090dAdr uint8 = 0x34
-	var ledgpiodAdr uint8 = 0x75
-
-	d := eeprom.Device{
-		BusIndex:   0,
-		BusAddress: 0x55,
+        d := eeprom.Device{
+                BusIndex:   0,
+                BusAddress: 0x55,
+        }
+        if err := d.GetInfo(); err != nil {
+		return err
 	}
-	d.GetInfo()
-	switch d.Fields.DeviceVersion {
-	case 0xff:
-		ucd9090dAdr = 0x7e
-		ledgpiodAdr = 0x22
-	case 0x00:
-		ucd9090dAdr = 0x7e
-		ledgpiodAdr = 0x22
-	default:
-		ucd9090dAdr = 0x34
-		ledgpiodAdr = 0x75
+	if d.Fields.ChassisType == TOR1 {
+		switch d.Fields.DeviceVersion {
+		case 0xff:
+			ucd9090dAdr = 0x7e
+			ledgpiodAdr = 0x22
+		case 0x00:
+			ucd9090dAdr = 0x7e
+			ledgpiodAdr = 0x22
+		default:
+			ucd9090dAdr = 0x34
+			ledgpiodAdr = 0x75
+		}
+		diagI2cTor()
+	} else if (d.Fields.ChassisType == CH1) && (d.Fields.BoardType == CH1MC) {
+		diagI2cCh1Mc()
+	} else if (d.Fields.ChassisType == CH1) && (d.Fields.BoardType == CH1MC) {
+                diagI2cCh1Lc()
 	}
 
+	return nil
+}
+
+func diagI2cTor() {
 	var r string
 	var result bool
 
@@ -262,7 +287,370 @@ func diagI2c() error {
 	fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "ping_fan_board", "-", result, i2cping_response_min, i2cping_response_max, r, "ping device 10x")
 
 	diagI2cWrite1Byte(0x01, 0x72, 0x00)
-	return nil
+}
+
+
+func diagI2cCh1Mc() {
+        var r string
+        var result bool
+
+	// avoid conflicts w/ interrupt handlers.
+        // i2c STOP
+        sd[0] = 0
+        j[0] = I{true, i2c.Write, 0, 0, sd, int(0x99), int(1), 0}
+        err := DoI2cRpc()
+        if err != nil {
+                log.Print(err)
+        }
+
+
+
+        fmt.Printf("\n%15s|%25s|%10s|%10s|%10s|%10s|%6s|%35s\n", "function", "parameter", "units", "value", "min", "max", "result", "description")
+        fmt.Printf("---------------|-------------------------|----------|----------|----------|----------|------|-----------------------------------\n")
+
+        /* diagTest: i2c mon
+        check that all i2c monitoring pins read high, stuck high pins will be discovered during i2c ping tests
+        */
+        pinstate, _ := gpioGet("BMC_I2C0_SCL_MON")
+        r = CheckPassB(pinstate, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "i2c1_scl_mon", "-", pinstate,
+		i2cmon_min, i2cmon_max, r, "check mon pin is high")
+
+        pinstate, _ = gpioGet("BMC_I2C0_SDA_MON")
+        r = CheckPassB(pinstate, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "i2c1_sda_mon", "-", pinstate,
+		i2cmon_min, i2cmon_max, r, "check mon pin is high")
+
+        pinstate, _ = gpioGet("BMC_I2C1_SCL_MON")
+        r = CheckPassB(pinstate, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "i2c2_scl_mon", "-", pinstate,
+		i2cmon_min, i2cmon_max, r, "check mon pin is high")
+
+        pinstate, _ = gpioGet("BMC_I2C1_SDA_MON")
+        r = CheckPassB(pinstate, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "i2c2_sda_mon", "-", pinstate,
+		i2cmon_min, i2cmon_max, r, "check mon pin is high")
+
+        // MC additional monitor pins
+        pinstate, _ = gpioGet("BMC_I2C2_SCL_MON")
+        r = CheckPassB(pinstate, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "i2c2_scl_mon", "-", pinstate,
+		i2cmon_min, i2cmon_max, r, "check mon pin is high")
+
+        pinstate, _ = gpioGet("BMC_I2C2_SDA_MON")
+        r = CheckPassB(pinstate, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "i2c2_sda_mon", "-", pinstate,
+		i2cmon_min, i2cmon_max, r, "check mon pin is high")
+
+
+
+        /* diagTest: host i2c
+        enable host access to main_i2c bus and check that bmc can access mfg eeprom on cpu card
+        repeat with fru_i2c bus
+        */
+        gpioSet("CPU_TO_MAIN_I2C_EN", true)
+        time.Sleep(50 * time.Millisecond)
+        result, _ = diagI2cPing(0x00, 0x51, 0x00, 1)
+        r = CheckPassB(result, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "cpu_to_main_i2c_en_on", "-", result,
+		i2cping_response_min, i2cping_response_max, r, "enable host bus, ping host eeprom")
+
+        gpioSet("CPU_TO_MAIN_I2C_EN", false)
+        time.Sleep(50 * time.Millisecond)
+        result, _ = diagI2cPing(0x00, 0x51, 0x00, 1)
+        r = CheckPassB(result, false)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "cpu_to_main_i2c_en_off", "-", result,
+		i2cping_noresponse_min, i2cping_noresponse_max, r, "disable host bus, ping host eeprom")
+        gpioSet("CPU_TO_FRU_I2C_EN", true)
+        time.Sleep(50 * time.Millisecond)
+        result, _ = diagI2cPing(0x01, 0x51, 0x00, 1)
+        r = CheckPassB(result, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "cpu_to_fru_i2c_en_on", "-", result,
+		i2cping_response_min, i2cping_response_max, r, "enable host bus, ping host eeprom")
+
+        gpioSet("CPU_TO_FRU_I2C_EN", false)
+        time.Sleep(50 * time.Millisecond)
+        result, _ = diagI2cPing(0x01, 0x51, 0x00, 1)
+        r = CheckPassB(result, false)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "cpu_to_fru_i2c_en_off", "-", result,
+		i2cping_noresponse_min, i2cping_noresponse_max, r, "disable host bus, ping host eeprom")
+        time.Sleep(100 * time.Millisecond)
+
+
+        /* diagTest: i2c power cycle
+        disable i2c power and check that i2c devices cannot be accessed
+        enable i2c power and check that i2c devices can be accessed
+        */
+        gpioSet("P3V3_I2C_EN", false)
+        gpioSet("CPU_TO_MAIN_I2C_EN", true)
+        time.Sleep(50 * time.Millisecond)
+        // MC pings the eeprom instead
+        result_s, _ := diagI2cPing(0x00, 0x55, 0x00, 1)
+        r_s := CheckPassB(result, false)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "p3v3_i2c_en_off", "-", result_s,
+		i2cping_noresponse_min, i2cping_noresponse_max, r_s, "disable i2c power, ping local eeprom")
+
+
+	gpioSet("P3V3_I2C_EN", true)
+        time.Sleep(50 * time.Millisecond)
+        // MC pings the eeprom instead
+        result, _ = diagI2cPing(0x00, 0x55, 0x00, 1)
+        r = CheckPassB(result, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "p3v3_i2c_en_on", "-", result,
+		i2cping_response_min, i2cping_response_max, r, "enable i2c power, ping local eeprom")
+        gpioSet("CPU_TO_MAIN_I2C_EN", false)
+
+
+        /* diagTest: i2c resets
+        activate i2c reset and validate associated devices cannot be accessed
+        */
+        gpioSet("LOCAL_I2C_RESET_L", false)
+        time.Sleep(50 * time.Millisecond)
+        result, _ = diagI2cPing(0x00, 0x71, 0x00, 1)   // MC's main_mux0
+        r = CheckPassB(result, false)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "main_i2c_mux_rst_l_on", "-", result,
+		i2cping_noresponse_min, i2cping_noresponse_max, r, "enable reset, ping main_mux0")
+
+        gpioSet("LOCAL_I2C_RESET_L", true)
+        time.Sleep(50 * time.Millisecond)
+        result, _ = diagI2cPing(0x00, 0x71, 0x00, 1)   // MC's main_mux0
+        r = CheckPassB(result, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "main_i2c_mux_rst_l_off", "-", result,
+		i2cping_response_min, i2cping_response_max, r, "disable reset, ping main_mux0")
+
+	gpioSet("FRU_I2C_RESET_L", false)
+        time.Sleep(50 * time.Millisecond)
+        result, _ = diagI2cPing(0x01, 0x70, 0x00, 1)    // MC's fru_mux0
+        r = CheckPassB(result, false)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "fru_i2c_mux_rst_l_on", "-", result,
+		i2cping_noresponse_min, i2cping_noresponse_max, r, "enable reset, ping fru_mux0")
+
+        gpioSet("FRU_I2C_RESET_L", true)
+        time.Sleep(50 * time.Millisecond)
+        result, _ = diagI2cPing(0x01, 0x70, 0x00, 1)    // MC's fru_mux0
+        r = CheckPassB(result, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "fru_i2c_mux_rst_l_off", "-", result,
+		i2cping_response_min, i2cping_response_max, r, "disable reset, ping fru_mux0")
+
+
+        /* diagTest: P3V3_FAN_EN power cycle
+                disable fan board 3.3V power and check that i2c devices cannot be accessed
+                enable fan board 3.3Vpower and check that i2c devices can be accessed
+        */
+/****
+	Fan diag TBD
+        gpioSet("P3V3_FAN_EN", false)
+        time.Sleep(50 * time.Millisecond)
+        diagI2cWrite1Byte(0x01, 0x72, 0x04)
+        time.Sleep(10 * time.Millisecond)
+        diagI2cWrite1Byte(0x01, 0x20, 0x00)
+        result, _ = diagI2cPing(0x01, 0x20, 0x00, 1)
+        r = CheckPassB(result, false)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "p3v3_fan_en_off", "-", result,
+		i2cping_response_min, i2cping_response_max, r, "disable fan3.3V, ping fan brd gpio")
+
+        gpioSet("P3V3_FAN_EN", true)
+        time.Sleep(50 * time.Millisecond)
+        diagI2cWrite1Byte(0x01, 0x20, 0x00)
+        result, _ = diagI2cPing(0x01, 0x20, 0x00, 1)
+        r = CheckPassB(result, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "p3v3_fan_en_on", "-", result,
+		i2cping_response_min, i2cping_response_max, r, "enable fan3.3V, ping fan brd gpio")
+
+        diagI2cWrite1Byte(0x01, 0x72, 0x00)
+*** */
+
+
+
+        /* diagTest: i2c devices
+        check all i2c devices are accessible
+        */
+        // MAIN_MUX0
+        //
+        result, _ = diagI2cPing(0x00, 0x71, 0x00, 10)
+        r = CheckPassB(result, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "ping_main_mux0", "-", result,
+		i2cping_response_min, i2cping_response_max, r, "ping device 10x")
+
+        diagI2cWrite1Byte(0x00, 0x71, 0x01)
+        time.Sleep(10 * time.Millisecond)
+        result, _ = diagI2cPing(0x00, 0x47, 0x00, 10)
+        r = CheckPassB(result, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "ping_LM25066", "-", result,
+		i2cping_response_min, i2cping_response_max, r, "ping device 10x")
+
+        diagI2cWrite1Byte(0x00, 0x71, 0x02)
+        time.Sleep(10 * time.Millisecond)
+        result, _ = diagI2cPing(0x00, 0x7e, 0x00, 10)
+        r = CheckPassB(result, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "ping_ucd9090", "-", result,
+		i2cping_response_min, i2cping_response_max, r, "ping device 10x")
+
+        diagI2cWrite1Byte(0x00, 0x71, 0x04)
+        time.Sleep(10 * time.Millisecond)
+        result, _ = diagI2cPing(0x00, 0x2c, 0x00, 10)
+        r = CheckPassB(result, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "ping_hwmonitor", "-", result,
+		i2cping_response_min, i2cping_response_max, r, "ping device 10x")
+
+        diagI2cWrite1Byte(0x00, 0x71, 0x08)
+        time.Sleep(10 * time.Millisecond)
+        result, _ = diagI2cPing(0x00, 0x48, 0x00, 10)
+        r = CheckPassB(result, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "ping_intake_tmp75", "-",
+                result, i2cping_response_min, i2cping_response_max, r, "ping device 10x")
+
+        diagI2cWrite1Byte(0x00, 0x71, 0x10)
+        time.Sleep(10 * time.Millisecond)
+        result, _ = diagI2cPing(0x00, 0x50, 0x00, 10)
+        r = CheckPassB(result, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "ping_qsfp_eeprom", "-",
+                result, i2cping_response_min, i2cping_response_max, r, "ping device 10x")
+
+
+        diagI2cWrite1Byte(0x00, 0x71, 0x20)
+        time.Sleep(10 * time.Millisecond)
+        diagI2cWrite1Byte(0x00, 0x26, 0x00)
+        result, _ = diagI2cPing(0x00, 0x26, 0x00, 10)
+        r = CheckPassB(result, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "ping_9534_main_gpio0", "-",
+                result, i2cping_response_min, i2cping_response_max, r, "ping device 10x")
+
+        diagI2cWrite1Byte(0x00, 0x71, 0x40)
+        time.Sleep(10 * time.Millisecond)
+        result, _ = diagI2cPing(0x00, 0x5e, 0x00, 10)
+        r = CheckPassB(result, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "ping_pex8780", "-",
+                result, i2cping_response_min, i2cping_response_max, r, "ping device 10x")
+
+        diagI2cWrite1Byte(0x00, 0x71, 0x00)
+	time.Sleep(10 * time.Millisecond)
+
+
+        // MAIN_MUX1
+        //
+        result, _ = diagI2cPing(0x00, 0x72, 0x00, 10)
+        r = CheckPassB(result, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "ping_main_mux1", "-",
+                result, i2cping_response_min, i2cping_response_max, r, "ping device 10x")
+
+        diagI2cWrite1Byte(0x00, 0x72, 0x01)
+        time.Sleep(10 * time.Millisecond)
+        result, _ = diagI2cPing(0x00, 0x76, 0x00, 10)
+        r = CheckPassB(result, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "ping_9539_main_gpio1", "-",
+                result, i2cping_response_min, i2cping_response_max, r, "ping device 10x")
+
+        diagI2cWrite1Byte(0x00, 0x72, 0x02)
+        time.Sleep(10 * time.Millisecond)
+        result, _ = diagI2cPing(0x00, 0x76, 0x00, 10)
+        r = CheckPassB(result, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "ping_9539_main_gpio2", "-",
+                result, i2cping_response_min, i2cping_response_max, r, "ping device 10x")
+
+        diagI2cWrite1Byte(0x00, 0x72, 0x04)
+        time.Sleep(10 * time.Millisecond)
+        result, _ = diagI2cPing(0x00, 0x76, 0x00, 10)
+        r = CheckPassB(result, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "ping_9539_main_gpio3", "-",
+                result, i2cping_response_min, i2cping_response_max, r, "ping device 10x")
+
+        diagI2cWrite1Byte(0x00, 0x72, 0x08)
+        time.Sleep(10 * time.Millisecond)
+        result, _ = diagI2cPing(0x00, 0x76, 0x00, 10)
+        r = CheckPassB(result, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "ping_9539_main_gpio4", "-",
+                result, i2cping_response_min, i2cping_response_max, r, "ping device 10x")
+
+        diagI2cWrite1Byte(0x00, 0x72, 0x10)
+        time.Sleep(10 * time.Millisecond)
+        result, _ = diagI2cPing(0x00, 0x76, 0x00, 10)
+        r = CheckPassB(result, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "ping_9539_main_gpio5", "-",
+                result, i2cping_response_min, i2cping_response_max, r, "ping device 10x")
+
+        diagI2cWrite1Byte(0x00, 0x72, 0x20)
+        time.Sleep(10 * time.Millisecond)
+        result, _ = diagI2cPing(0x00, 0x76, 0x00, 10)
+        r = CheckPassB(result, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "ping_9539_main_gpio6", "-",
+                result, i2cping_response_min, i2cping_response_max, r, "ping device 10x")
+
+        diagI2cWrite1Byte(0x00, 0x72, 0x40)
+        time.Sleep(10 * time.Millisecond)
+        result, _ = diagI2cPing(0x00, 0x76, 0x00, 10)
+        r = CheckPassB(result, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "ping_9539_main_gpio7", "-",
+                result, i2cping_response_min, i2cping_response_max, r, "ping device 10x")
+
+        diagI2cWrite1Byte(0x00, 0x72, 0x80)
+        time.Sleep(10 * time.Millisecond)
+        diagI2cWrite1Byte(0x00, 0x75, 0x00)
+        result, _ = diagI2cPing(0x00, 0x75, 0x00, 10)
+        r = CheckPassB(result, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "ping_9534_led_gpio0", "-",
+                result, i2cping_response_min, i2cping_response_max, r, "ping device 10x")
+
+        diagI2cWrite1Byte(0x00, 0x72, 0x00)
+	time.Sleep(10 * time.Millisecond)
+
+
+        // FRU_MUX0
+        //
+        result, _ = diagI2cPing(0x01, 0x70, 0x00, 10)
+        r = CheckPassB(result, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "ping_fru_mux0", "-",
+                result, i2cping_response_min, i2cping_response_max, r, "ping device 10x")
+
+        diagI2cWrite1Byte(0x01, 0x70, 0x01)
+        time.Sleep(10 * time.Millisecond)
+        result, _ = diagI2cPing(0x01, 0x73, 0x00, 10)
+        r = CheckPassB(result, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "ping_fru_mux1", "-",
+                result, i2cping_response_min, i2cping_response_max, r, "ping device 10x")
+
+        diagI2cWrite1Byte(0x01, 0x70, 0x02)
+        time.Sleep(10 * time.Millisecond)
+        result, _ = diagI2cPing(0x01, 0x73, 0x00, 10)
+        r = CheckPassB(result, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "ping_fru_mux2", "-",
+                result, i2cping_response_min, i2cping_response_max, r, "ping device 10x")
+
+        diagI2cWrite1Byte(0x01, 0x70, 0x04)
+        time.Sleep(10 * time.Millisecond)
+        result, _ = diagI2cPing(0x01, 0x73, 0x00, 10)
+        r = CheckPassB(result, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "ping_fru_mux3", "-",
+                result, i2cping_response_min, i2cping_response_max, r, "ping device 10x")
+
+        diagI2cWrite1Byte(0x01, 0x70, 0x08)
+        time.Sleep(10 * time.Millisecond)
+        result, _ = diagI2cPing(0x01, 0x73, 0x00, 10)
+        r = CheckPassB(result, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "ping_fru_mux4", "-",
+                result, i2cping_response_min, i2cping_response_max, r, "ping device 10x")
+
+        diagI2cWrite1Byte(0x01, 0x70, 0x10)
+        time.Sleep(10 * time.Millisecond)
+        result, _ = diagI2cPing(0x01, 0x73, 0x00, 10)
+        r = CheckPassB(result, true)
+        fmt.Printf("%15s|%25s|%10s|%10t|%10t|%10t|%6s|%35s\n", "i2c", "ping_fru_mux5", "-",
+                result, i2cping_response_min, i2cping_response_max, r, "ping device 10x")
+
+        diagI2cWrite1Byte(0x01, 0x70, 0x00)
+
+
+        //i2c START
+        sd[0] = 0
+        j[0] = I{true, i2c.Write, 0, 0, sd, int(0x99), int(0), 0}
+        err = DoI2cRpc()
+        if err != nil {
+                log.Print(err)
+        }
+}
+
+func diagI2cCh1Lc() {
+
 }
 
 func diagSwitchConsole() error {
