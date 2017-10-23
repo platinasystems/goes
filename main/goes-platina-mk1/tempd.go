6b7594453fba38131c3a6dc5245c8180c9b5ef9a
Jason Pang <jason@platinasystems.com>
2017-10-23 09:45:23 -0700

    Forgot to add a tempd file
    Signed-off-by: Jason Pang <jason@platinasystems.com>

---
+++ main/goes-platina-mk1/tempd.go
@@ -0,0 +1,17 @@
+// Copyright © 2015-2016 Platina Systems, Inc. All rights reserved.
+// Use of this source code is governed by the GPL-2 license described in the
+// LICENSE file.
+
+package main
+
+import (
+	"github.com/platinasystems/go/goes/cmd/tempd"
+)
+
+func init() {
+	tempd.Init = func() {
+		tempd.VpageByKey = map[string]uint8{
+			"sys.cpu.coretemp.units.C": 0,
+		}
+	}
+}
