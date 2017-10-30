0d4138e348fb8db60521aacfe44eb5e05fadd63b
Mark Pleso <mpleso@platinasystems.com>
2017-10-30 12:53:52 -0700

    bmc: add eth0 ip address redis key
    Signed-off-by: Mark Pleso <mpleso@platinasystems.com>

--- main/goes-platina-mk2-mc1-bmc/start.go
+++ main/goes-platina-mk2-mc1-bmc/start.go
@@ -6,12 +6,16 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
+	"syscall"
 	"time"
 
 	"github.com/platinasystems/go/goes/cmd/platina/mk1/bmc/ipcfg"
 	"github.com/platinasystems/go/goes/cmd/start"
 	"github.com/platinasystems/go/internal/gpio"
 	"github.com/platinasystems/go/internal/log"
+	"github.com/platinasystems/go/internal/redis/publisher"
 )
 
 func init() {
@@ -47,8 +51,72 @@ func init() {
 			time.Sleep(1 * time.Microsecond)
 			pin.SetValue(true)
 		}
+		err := pubEth0()
+		if err != nil {
+			return err
+		}
 		ipcfg.UpdateEnv(false)
 		ipcfg.UpdateEnv(true)
 		return nil
 	}
+}
+
+func pubEth0() (err error) {
+	var last, kmsg log.Kmsg
+	f, err := os.Open("/dev/kmsg")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	if err = syscall.SetNonblock(int(f.Fd()), true); err != nil {
+		return err
+	}
+	buf := make([]byte, 4096)
+	defer func() { buf = buf[:0] }()
+	for {
+		n, err := f.Read(buf)
+		if err != nil {
+			break
+		}
+		kmsg.Parse(buf[:n])
+		if last.Seq == 0 || last.Seq < kmsg.Seq {
+			if strings.Contains(kmsg.Msg, "init.redisd") {
+				if strings.Contains(kmsg.Msg, "eth0") {
+					err := pubAddr(kmsg.Msg)
+					if err != nil {
+						return err
+					}
+				}
+				last.Seq = kmsg.Seq
+			}
+		}
+	}
+	return nil
+}
+
+func pubAddr(s string) (err error) {
+	ip := strings.SplitAfter(s, "[")
+	i := ip[2]
+	ip = strings.Split(i, "%")
+	if strings.Contains(s, "::") {
+		err = pubKey("eth0.ipv6", ip[0])
+		if err != nil {
+			return err
+		}
+	} else {
+		err = pubKey("eth0.ipv4", ip[0])
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func pubKey(k string, v interface{}) (err error) {
+	var pub *publisher.Publisher
+	if pub, err = publisher.New(); err != nil {
+		return err
+	}
+	pub.Print(k, ": ", v)
+	return nil
 }
