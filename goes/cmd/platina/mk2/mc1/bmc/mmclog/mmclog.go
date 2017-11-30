08abc46b112cba822dfc78e21d8d1212f62e20d3
Mark Pleso <mpleso@platinasystems.com>
2017-11-30 10:50:03 -0800

    mk2-lc1-bmc mk2-mc1-bmc: add mmclog command
    Signed-off-by: Mark Pleso <mpleso@platinasystems.com>

--- goes/cmd/platina/mk2/mc1/bmc/mmclog/mmclog.go
+++ goes/cmd/platina/mk2/mc1/bmc/mmclog/mmclog.go
@@ -5,20 +5,34 @@
 package mmclog
 
 import (
-	//"fmt"
-	//"strconv"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
 
-	"github.com/platinasystems/go/goes/cmd/platina/mk2/mc1/bmc/mmclogd"
 	"github.com/platinasystems/go/goes/lang"
+	"github.com/platinasystems/go/internal/flags"
+	"github.com/platinasystems/go/internal/parms"
 )
 
 const (
 	Name    = "mmclog"
-	Apropos = "MMC persistant dmesg log status"
-	Usage   = "mmclog"
+	Apropos = "display persistant MMC dmesg log"
+	Usage   = "mmclog [-l LINE] [-c COUNT] -2"
 	Man     = `
 DESCRIPTION
-        The mmclog command shows status of MMC dmesg logs`
+        The mmclog command displays MMC dmesg log
+
+	The -l parameter specifies starting line number.
+	The -c parameter specifies number of lines to display.
+	The -2 flag displays the secondary(older) dmesg log, if available.
+
+	The default is to display the last 25 lines of the primary log.`
+
+	LOGA      = "/mnt/dmesg.txt"
+	LOGB      = "/mnt/dmesg2.txt"
+	DfltLine  = "0"
+	DfltCount = "25"
 )
 
 type Interface interface {
@@ -48,37 +62,79 @@ var (
 )
 
 func (cmd) Main(args ...string) (err error) {
-	/*var a uint64 = 0
-	if len(args) != 0 {
-		a, err = strconv.ParseUint(args[0], 10, 32)
-		if err != nil {
-			return fmt.Errorf("%s: %v", args[0], err)
-		}
+	flag, args := flags.New(args, "-2")
+	parm, args := parms.New(args, "-l", "-c")
+	if len(parm.ByName["-l"]) == 0 {
+		parm.ByName["-l"] = DfltLine
+	}
+	if len(parm.ByName["-c"]) == 0 {
+		parm.ByName["-c"] = DfltCount
+	}
+	log := LOGA
+	if flag.ByName["-2"] {
+		log = LOGB
+	}
+	if _, err := os.Stat(log); os.IsNotExist(err) {
+		fmt.Println("log file: ", log, "does not exist")
+		return nil
 	}
 
-	switch a {
-	case 1:
-		return mmclogd.LogDmesg(1)
-	case 2:
-		return mmclogd.LogDmesg(100000)
-	case 3:
-		return mmclogd.StartTicker()
-	case 4:
-		return mmclogd.StopTicker()
-	case 5:
-		return mmclogd.MountMMC()
-	case 6:
-		return mmclogd.ListMMC()
-	case 7:
-		return mmclogd.InitLogging()
-	case 8:
-		return mmclogd.GetDmesgInfo()
-	case 9:
-		return mmclogd.ReadFile()
-	default:
-		return mmclogd.Status()
+	max := 0
+	line := 0
+	count := 0
+	if max, err = dspSiz(log); err != nil {
+		return err
+	}
+	if line, err = strconv.Atoi(parm.ByName["-l"]); err != nil {
+		return err
+	}
+	if count, err = strconv.Atoi(parm.ByName["-c"]); err != nil {
+		return err
+	}
+	if line == 0 {
+		line = max - count + 1
+	}
+	if err = dspLog(log, line, count); err != nil {
+		return err
+	}
 	return nil
-	}*/
+}
 
-	return mmclogd.Status()
+func dspLog(log string, line int, count int) (err error) {
+	f, err := os.Open(log)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	thisLine := 0
+	sc := bufio.NewScanner(f)
+	for sc.Scan() {
+		thisLine++
+		if thisLine >= line && thisLine < (line+count) {
+			fmt.Println(thisLine, sc.Text())
+		}
+	}
+	f.Close()
+	return nil
+}
+
+func dspSiz(logname string) (max int, err error) {
+	f, err := os.Open(logname)
+	if err != nil {
+		return 0, err
+	}
+	defer f.Close()
+	fi, err := f.Stat()
+	if err != nil {
+		return 0, err
+	}
+	siz := fi.Size()
+	sc := bufio.NewScanner(f)
+	cnt := 0
+	for sc.Scan() {
+		cnt++
+	}
+	f.Close()
+	fmt.Println("\nlog: ", logname, "  size: ", siz, "  lines: ", cnt)
+	return cnt, nil
 }
