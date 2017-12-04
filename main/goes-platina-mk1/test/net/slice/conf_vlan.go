798094b81b78e60a8efb03a8b5710757ed1b894d
Stig Thormodsrud <stig@platinasystems.com>
2017-12-04 15:27:37 -0800

    change net/slice test to use same cable as all the other vlan tests
    Signed-off-by: Stig Thormodsrud <stig@platinasystems.com>

--- main/goes-platina-mk1/test/net/slice/conf_vlan.go
+++ main/goes-platina-mk1/test/net/slice/conf_vlan.go
@@ -12,61 +12,61 @@ routers:
 - hostname: CA-1
   cmd: "/root/startup.sh"
   intfs:
-  - name: {{index . 3 0}}
+  - name: {{index . 0 0}}
     address: 10.1.0.1/24
     vlan: 10
 - hostname: RA-1
   cmd: "/root/startup.sh"
   intfs:
-  - name: {{index . 3 1}}
+  - name: {{index . 0 1}}
     address: 10.1.0.2/24
     vlan: 10
-  - name: {{index . 3 0}}
+  - name: {{index . 0 0}}
     address: 10.2.0.2/24
     vlan: 20
 - hostname: RA-2
   cmd: "/root/startup.sh"
   intfs:
-  - name: {{index . 3 1}}
+  - name: {{index . 0 1}}
     address: 10.2.0.3/24
     vlan: 20
-  - name: {{index . 3 0}}
+  - name: {{index . 0 0}}
     address: 10.3.0.3/24
     vlan: 30
 - hostname: CA-2
   cmd: "/root/startup.sh"
   intfs:
-  - name: {{index . 3 1}}
+  - name: {{index . 0 1}}
     address: 10.3.0.4/24
     vlan: 30
 - hostname: CB-1
   cmd: "/root/startup.sh"
   intfs:
-  - name: {{index . 3 0}}
+  - name: {{index . 0 0}}
     address: 10.1.0.1/24
     vlan: 40
 - hostname: RB-1
   cmd: "/root/startup.sh"
   intfs:
-  - name: {{index . 3 1}}
+  - name: {{index . 0 1}}
     address: 10.1.0.2/24
     vlan: 40
-  - name: {{index . 3 0}}
+  - name: {{index . 0 0}}
     address: 10.2.0.2/24
     vlan: 50
 - hostname: RB-2
   cmd: "/root/startup.sh"
   intfs:
-  - name: {{index . 3 1}}
+  - name: {{index . 0 1}}
     address: 10.2.0.3/24
     vlan: 50
-  - name: {{index . 3 0}}
+  - name: {{index . 0 0}}
     address: 10.3.0.3/24
     vlan: 60
 - hostname: CB-2
   cmd: "/root/startup.sh"
   intfs:
-  - name: {{index . 3 1}}
+  - name: {{index . 0 1}}
     address: 10.3.0.4/24
     vlan: 60
 `
