bf8c01457cfb6f3c4a652cbeefe6a5b46505000d
Stig Thormodsrud <stig@platinasystems.com>
2017-12-11 11:26:36 -0800

    add punt path iperf3 stress
    Signed-off-by: Stig Thormodsrud <stig@platinasystems.com>

--- main/goes-platina-mk1/test/net/static/static.go
+++ main/goes-platina-mk1/test/net/static/static.go
@@ -28,6 +28,9 @@ func Test(t *testing.T, source []byte) {
 		{"frr", checkFrr},
 		{"routes", checkRoutes},
 		{"inter-connectivity", checkInterConnectivity},
+		{"flap", checkFlap},
+		{"inter-connectivity2", checkInterConnectivity2},
+		{"punt-stress", checkPuntStress},
 	}.Run(t)
 }
 
@@ -50,7 +53,6 @@ func checkConnectivity(t *testing.T) {
 		out, err := docker.ExecCmd(t, x.hostname, config, cmd)
 		assert.Nil(err)
 		assert.Match(out, "[1-3] packets received")
-
 	}
 }
 
@@ -102,6 +104,7 @@ func checkInterConnectivity(t *testing.T) {
 		{"CA-1", "10.3.0.4"},
 		{"CA-2", "10.1.0.1"},
 	} {
+		t.Logf("ping from %v to %v", x.hostname, x.target)
 		cmd := []string{"ping", "-c3", x.target}
 		out, err := docker.ExecCmd(t, x.hostname, config, cmd)
 		assert.Nil(err)
@@ -109,4 +112,82 @@ func checkInterConnectivity(t *testing.T) {
 		assert.Program(test.Self{},
 			"vnet", "show", "ip", "fib", "table", x.hostname)
 	}
+}
+
+func checkFlap(t *testing.T) {
+	assert := test.Assert{t}
+
+	for _, r := range config.Routers {
+		for _, i := range r.Intfs {
+			var intf string
+			if i.Vlan != "" {
+				intf = i.Name + "." + i.Vlan
+			} else {
+				intf = i.Name
+			}
+			cmd := []string{"ip", "link", "set", "down", intf}
+			_, err := docker.ExecCmd(t, r.Hostname, config, cmd)
+			assert.Nil(err)
+			time.Sleep(1 * time.Second)
+			cmd = []string{"ip", "link", "set", "up", intf}
+			_, err = docker.ExecCmd(t, r.Hostname, config, cmd)
+			assert.Nil(err)
+			time.Sleep(1 * time.Second)
+			assert.Program(test.Self{}, "vnet", "show", "ip", "fib")
+		}
+	}
+}
+
+func checkInterConnectivity2(t *testing.T) {
+	assert := test.Assert{t}
+
+	for _, x := range []struct {
+		hostname string
+		target   string
+	}{
+		{"CA-1", "10.1.0.2"},
+		{"RA-1", "10.1.0.1"},
+		{"RA-1", "10.2.0.3"},
+		{"RA-2", "10.2.0.2"},
+		{"RA-2", "10.3.0.4"},
+		{"CA-2", "10.3.0.3"},
+		{"CA-1", "10.3.0.4"},
+		{"CA-2", "10.1.0.1"},
+	} {
+		t.Logf("ping from %v to %v", x.hostname, x.target)
+		cmd := []string{"ping", "-c3", x.target}
+		out, err := docker.ExecCmd(t, x.hostname, config, cmd)
+		assert.Nil(err)
+		assert.Match(out, "[1-3] packets received")
+		assert.Program(test.Self{},
+			"vnet", "show", "ip", "fib", "table", x.hostname)
+	}
+}
+
+func checkPuntStress(t *testing.T) {
+	assert := test.Assert{t}
+
+	t.Log("Check punt stress with iperf3")
+
+	done := make(chan bool, 1)
+
+	go func(done chan bool) {
+		cmd := []string{"timeout", "15", "iperf3", "-s"}
+		docker.ExecCmd(t, "CA-2", config, cmd)
+		done <- true
+	}(done)
+
+	time.Sleep(1 * time.Second)
+	cmd := []string{"iperf3", "-c", "10.3.0.4"}
+	out, err := docker.ExecCmd(t, "CA-1", config, cmd)
+
+	r, err := regexp.Compile(`([0-9\.]+)\s+Gbits/sec\s+receiver`)
+	assert.Nil(err)
+	result := r.FindStringSubmatch(out)
+	if len(result) == 2 {
+		t.Logf("iperf3 - %v Gbits/sec", result[1])
+	} else {
+		t.Logf("iperf3 regex failed to find rate [%v]", out)
+	}
+	<-done
 }
