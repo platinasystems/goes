2d9dabc0b292ddbb66e9264b3edd8f01a9e6c17c
Saptak Takalkar <saptak.takalkar@calsoftinc.com>
2018-01-03 23:23:14 -0800

    Added playbook and module for bird peering route advertise test cases

---
+++ ansible/regtest/library/testcases/bgp/test_bird_peering_route_advertise.py
@@ -0,0 +1,233 @@
+#!/usr/bin/python
+""" Test/Verify BIRD PEERING ROUTE ADVERTISE """
+
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible. If not, see <http://www.gnu.org/licenses/>.
+#
+
+import shlex
+
+from collections import OrderedDict
+
+from ansible.module_utils.basic import AnsibleModule
+
+DOCUMENTATION = """
+---
+module: test_bird_peering_route_advertise
+author: Platina Systems
+short_description: Module to test and verify bird configuration.
+description:
+    Module to test and verify bird configurations and log the same.
+options:
+    switch_name:
+      description:
+        - Name of the switch on which tests will be performed.
+      required: False
+      type: str
+    package_name:
+      description:
+        - Name of the package installed (e.g. quagga/frr/bird).
+      required: False
+      type: str
+    spine_list:
+      description:
+        - List of all spine switches.
+      required: False
+      type: list
+      default: []
+    leaf_list:
+      description:
+        - List of all leaf switches.
+      required: False
+      type: list
+      default: []
+    is_ibgp:
+      description:
+        - Flag to indicate if we need to verify ibgp configurations.
+      required: False
+      type: bool
+      default: False
+    hash_name:
+      description:
+        - Name of the hash in which to store the result in redis.
+      required: False
+      type: str
+    log_dir_path:
+      description:
+        - Path to log directory where logs will be stored.
+      required: False
+      type: str
+"""
+
+EXAMPLES = """
+- name: Verify bird peering route advertise
+  test_bird_peering_route_advertise:
+    switch_name: "{{ inventory_hostname }}"
+    hash_name: "{{ hostvars['server_emulator']['hash_name'] }}"
+    log_dir_path: "{{ log_dir_path }}"
+"""
+
+RETURN = """
+hash_dict:
+  description: Dictionary containing key value pairs to store in hash.
+  returned: always
+  type: dict
+"""
+
+RESULT_STATUS = True
+HASH_DICT = OrderedDict()
+
+
+def run_cli(module, cli):
+    """
+    Method to execute the cli command on the target node(s) and
+    returns the output.
+    :param module: The Ansible module to fetch input parameters.
+    :param cli: The complete cli string to be executed on the target node(s).
+    :return: Output/Error or None depending upon the response from cli.
+    """
+    cli = shlex.split(cli)
+    rc, out, err = module.run_command(cli)
+
+    if out:
+        return out.rstrip()
+    elif err:
+        return err.rstrip()
+    else:
+        return None
+
+
+def execute_commands(module, cmd):
+    """
+    Method to execute given commands and return the output.
+    :param module: The Ansible module to fetch input parameters.
+    :param cmd: Command to execute.
+    :return: Output of the commands.
+    """
+    global HASH_DICT
+
+    if 'service' in cmd and 'restart' in cmd:
+        out = None
+    else:
+        out = run_cli(module, cmd)
+
+    # Store command prefixed with exec time as key and
+    # command output as value in the hash dictionary
+    exec_time = run_cli(module, 'date +%Y%m%d%T')
+    key = '{0} {1} {2}'.format(module.params['switch_name'], exec_time, cmd)
+    HASH_DICT[key] = out
+
+    return out
+
+
+def verify_bird_peering(module):
+    """
+    Method to verify bird peering.
+    :param module: The Ansible module to fetch input parameters.
+    """
+    global RESULT_STATUS, HASH_DICT
+    failure_summary = ''
+    dummy_interfaces_list = []
+    switch_name = module.params['switch_name']
+    is_ibgp = module.params['is_ibgp']
+    package_name = module.params['package_name']
+    spine_list = module.params['spine_list']
+    leaf_list = module.params['leaf_list']
+
+    # Get the current/running configurations
+    execute_commands(module, 'cat /etc/bird/bird.conf')
+
+    # Restart and check package status
+    execute_commands(module, 'service {} restart'.format(package_name))
+    execute_commands(module, 'service {} status'.format(package_name))
+
+    if not is_ibgp:
+        switches_list = spine_list + leaf_list
+    else:
+        is_spine = True if switch_name in spine_list else False
+        
+        if is_spine:
+            switches_list = leaf_list
+        else:
+            switches_list = spine_list
+            
+        switches_list.append(switch_name)
+
+    # Get the list of dummy interfaces to verify
+    for switch in switches_list:
+        ip = '192.168.{}.0'.format(switch[-2::])
+        dummy_interfaces_list.append(ip)
+
+    cmd = 'birdc show route all'
+    all_routes = execute_commands(module, cmd)
+
+    for ip in dummy_interfaces_list:
+        if ip not in all_routes:
+            RESULT_STATUS = False
+            failure_summary += 'On switch {} '.format(switch_name)
+            failure_summary += 'dummy interface {} '.format(ip)
+            failure_summary += 'is not present in the output '
+            failure_summary += 'of command {}\n'.format(cmd)
+
+    HASH_DICT['result.detail'] = failure_summary
+
+    # Get the GOES status info
+    execute_commands(module, 'goes status')
+
+
+def main():
+    """ This section is for arguments parsing """
+    module = AnsibleModule(
+        argument_spec=dict(
+            switch_name=dict(required=False, type='str'),
+            package_name=dict(required=False, type='str'),
+            spine_list=dict(required=False, type='list', default=[]),
+            leaf_list=dict(required=False, type='list', default=[]),
+            is_ibgp=dict(required=False, type='bool', default=False),
+            hash_name=dict(required=False, type='str'),
+            log_dir_path=dict(required=False, type='str'),
+        )
+    )
+
+    global HASH_DICT, RESULT_STATUS
+
+    verify_bird_peering(module)
+
+    # Calculate the entire test result
+    HASH_DICT['result.status'] = 'Passed' if RESULT_STATUS else 'Failed'
+
+    # Create a log file
+    log_file_path = module.params['log_dir_path']
+    log_file_path += '/{}.log'.format(module.params['hash_name'])
+    log_file = open(log_file_path, 'w')
+    for key, value in HASH_DICT.iteritems():
+        log_file.write(key)
+        log_file.write('\n')
+        log_file.write(str(value))
+        log_file.write('\n')
+        log_file.write('\n')
+
+    log_file.close()
+
+    # Exit the module and return the required JSON.
+    module.exit_json(
+        hash_dict=HASH_DICT,
+        log_file_path=log_file_path
+    )
+
+if __name__ == '__main__':
+    main()
+
