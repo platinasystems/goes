16195e7e39f556e977d8e003719ad3095ba1f1c9
Saptak Takalkar <saptak.takalkar@calsoftinc.com>
2018-02-13 02:21:35 -0800

    Added playbooks for vlan config lldp and on transit vlan interfaces

--- ansible/regtest/library/testcases/vlan/test_vlan_configuration.py
+++ ansible/regtest/library/testcases/vlan/test_vlan_configuration.py
@@ -61,6 +61,12 @@ options:
       required: False
       type: bool
       default: False
+    lldp:
+      description:
+        - Flag to indicate if lldp needs to be used.
+      required: False
+      type: bool
+      default: False
     multiple_vlan:
       description:
         - Flag to indicate if multiple vlans for same interface need to be configured.
@@ -152,6 +158,7 @@ def verify_vlan_configurations(module):
     leaf_switch = module.params['leaf_switch']
     eth_list = module.params['eth_list'].split(',')
     arping = module.params['arping']
+    lldp = module.params['lldp']
     multiple_vlan = module.params['multiple_vlan']
     third_octet = switch_name[-2::]
 
@@ -210,65 +217,82 @@ def verify_vlan_configurations(module):
                         failure_summary += 'failed to configure vlan on interface '
                         failure_summary += 'eth-{}-1.{}\n'.format(eth, subport)
 
-    # Initiate arping/ping and verify tcpdump output
-    if is_leaf:
-        for eth in eth_list:
-            index = eth_list.index(eth)
-            last_octet = spine_list[index][-2::]
-            if arping:
-                arp_cmd = 'arping -C 15 -I eth-{}-1.1 192.168.{}.{}'.format(
-                    eth, eth, last_octet
-                )
-                execute_commands(module, arp_cmd)
-            else:
-                if not multiple_vlan:
-                    ping_cmd = 'ping -c 15 192.168.{}.{}'.format(eth, last_octet)
-                    execute_commands(module, ping_cmd)
+    if not lldp:
+        # Initiate arping/ping and verify tcpdump output
+        if is_leaf:
+            for eth in eth_list:
+                index = eth_list.index(eth)
+                last_octet = spine_list[index][-2::]
+                if arping:
+                    arp_cmd = 'arping -C 15 -I eth-{}-1.1 192.168.{}.{}'.format(
+                        eth, eth, last_octet
+                    )
+                    execute_commands(module, arp_cmd)
                 else:
-                    vlan_id = int(eth)
-                    for subport in range(1, 5):
-                        vlan_id += 1
-                        ping_cmd = 'ping -c 10 192.168.{}.{}'.format(vlan_id, last_octet)
+                    if not multiple_vlan:
+                        ping_cmd = 'ping -c 15 192.168.{}.{}'.format(eth, last_octet)
                         execute_commands(module, ping_cmd)
-    else:
-        # Verify if vlan tagged packets are captured in tcpdump file
-        index = spine_list.index(switch_name)
-        eth = eth_list[index]
-        state = 'arp' if arping else 'icmp'
-
-        if not multiple_vlan:
-            cmd = 'tcpdump -c 15 -net -i eth-{}-1 {}'.format(eth, state)
-            tcpdump_out = execute_commands(module, cmd)
-
-            if tcpdump_out:
-                if ('802.1Q (0x8100)' not in tcpdump_out or
-                        'vlan {}'.format(eth) not in tcpdump_out):
-                    RESULT_STATUS = False
-                    failure_summary += 'On switch {} '.format(switch_name)
-                    failure_summary += 'there are no vlan tagged packets '
-                    failure_summary += 'captured for eth-{}-1\n'.format(eth)
-            else:
-                RESULT_STATUS = False
-                failure_summary += 'On switch {} '.format(switch_name)
-                failure_summary += 'failed to capture tcpdump output\n'
+                    else:
+                        vlan_id = int(eth)
+                        for subport in range(1, 5):
+                            vlan_id += 1
+                            ping_cmd = 'ping -c 10 192.168.{}.{}'.format(vlan_id, last_octet)
+                            execute_commands(module, ping_cmd)
         else:
-            vlan_id = int(eth)
-            for subport in range(1, 5):
-                vlan_id += 1
+            # Verify if vlan tagged packets are captured in tcpdump file
+            index = spine_list.index(switch_name)
+            eth = eth_list[index]
+            state = 'arp' if arping else 'icmp'
+
+            if not multiple_vlan:
                 cmd = 'tcpdump -c 15 -net -i eth-{}-1 {}'.format(eth, state)
                 tcpdump_out = execute_commands(module, cmd)
 
                 if tcpdump_out:
                     if ('802.1Q (0x8100)' not in tcpdump_out or
-                            'vlan {}'.format(vlan_id) not in tcpdump_out):
+                            'vlan {}'.format(eth) not in tcpdump_out):
                         RESULT_STATUS = False
                         failure_summary += 'On switch {} '.format(switch_name)
                         failure_summary += 'there are no vlan tagged packets '
-                        failure_summary += 'captured for eth-{}-1\n'.format(eth)
+                        failure_summary += 'captured in tcpdump for eth-{}-1\n'.format(eth)
                 else:
                     RESULT_STATUS = False
                     failure_summary += 'On switch {} '.format(switch_name)
                     failure_summary += 'failed to capture tcpdump output\n'
+            else:
+                vlan_id = int(eth)
+                for subport in range(1, 5):
+                    vlan_id += 1
+                    cmd = 'tcpdump -c 15 -net -i eth-{}-1 {}'.format(eth, state)
+                    tcpdump_out = execute_commands(module, cmd)
+
+                    if tcpdump_out:
+                        if ('802.1Q (0x8100)' not in tcpdump_out or
+                                'vlan {}'.format(vlan_id) not in tcpdump_out):
+                            RESULT_STATUS = False
+                            failure_summary += 'On switch {} '.format(switch_name)
+                            failure_summary += 'there are no vlan tagged packets '
+                            failure_summary += 'captured in tcpdump for eth-{}-1\n'.format(eth)
+                    else:
+                        RESULT_STATUS = False
+                        failure_summary += 'On switch {} '.format(switch_name)
+                        failure_summary += 'failed to capture tcpdump output\n'
+    else:
+        for eth in eth_list:
+            cmd = 'tcpdump -c 7 -G 10 -net -i eth-{}-1 not proto ospf and not arp -vvv'.format(eth)
+            lldp_out = execute_commands(module, cmd)
+
+            if lldp_out:
+                if ('vlan name: eth-{}-1.1'.format(eth) not in lldp_out or
+                        'vlan id (VID): {}'.format(eth) not in lldp_out):
+                    RESULT_STATUS = False
+                    failure_summary += 'On switch {} '.format(switch_name)
+                    failure_summary += 'there are no vlan tagged packets '
+                    failure_summary += 'captured in tcpdump for eth-{}-1\n'.format(eth)
+            else:
+                RESULT_STATUS = False
+                failure_summary += 'On switch {} '.format(switch_name)
+                failure_summary += 'failed to capture tcpdump output\n'
 
     HASH_DICT['result.detail'] = failure_summary
 
@@ -285,6 +309,7 @@ def main():
             leaf_switch=dict(required=False, type='str'),
             eth_list=dict(required=False, type='str'),
             arping=dict(required=False, type='bool', default=False),
+            lldp=dict(required=False, type='bool', default=False),
             multiple_vlan=dict(required=False, type='bool', default=False),
             hash_name=dict(required=False, type='str'),
             log_dir_path=dict(required=False, type='str'),
