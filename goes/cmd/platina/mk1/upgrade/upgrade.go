3b8c6013cee842ceaa96d655000c1d5b20bdf4e6
Mark Pleso <mpleso@platinasystems.com>
2018-09-17 14:33:06 -0700

    upgrade: remove bmc qsfp option from upgrade command
    Signed-off-by: Mark Pleso <mpleso@platinasystems.com>

--- goes/cmd/platina/mk1/upgrade/upgrade.go
+++ goes/cmd/platina/mk1/upgrade/upgrade.go
@@ -74,7 +74,6 @@ OPTIONS
 	-k                upgrade kernel
 	-c                upgrade coreboot
 	-a                upgrade all
-	-1                upgrade QSPI1(recovery QSPI), default is QSPI0
 	-f                force upgrade (ignore version check)`,
 	}
 }
