6926dc9a69b5e35f7da7efae8d2451bfece86356
mattgeddes-platina <mgeddes@platinasystems.com>
2018-12-26 18:57:55 -0800

    Added Platina Edge Device Users Guide first pass.

---
+++ docs/generate-toc.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+while($line=<STDIN>) {
+    chomp $line;
+
+    if($line =~ /^#/) {
+	# We have a section heading
+	#$indent = $line =~ tr/#//;
+	$indent = $line;
+	$indent =~ s/[^#]//g;	# remove all non-hash chars
+	$indent =~ s/#//;	# Eat the first one (ie, no indentation)
+	$indent =~ s/#/  /g;	# Indent based on the number of hashes
+	$line =~ s/#//g;
+	$anchor = lc($line);
+	$anchor =~ s/[^a-zA-Z0-9]//g;
+	print "${indent}1. [$line](#$anchor)\n";
+    }
+
+    #print "$line\n";
+}
+
