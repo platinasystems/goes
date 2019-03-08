59dd72210c31c1b7a02d44c6f8dacefc922f8830
Kevin Paul Herbert <kph@platinasystems.com>
2019-03-08 07:25:38 -0800

    reboot: Unmount filesystems
    Unmount filesystems before reboot.
    
    Signed-off-by: Kevin Paul Herbert <kph@platinasystems.com>

--- cmd/reboot/reboot.go
+++ cmd/reboot/reboot.go
@@ -7,11 +7,14 @@ package reboot
 import (
 	"syscall"
 
-	"github.com/platinasystems/goes/lang"
+	"github.com/platinasystems/goes"
 	"github.com/platinasystems/goes/internal/kexec"
+	"github.com/platinasystems/goes/lang"
 )
 
-type Command struct{}
+type Command struct {
+	g *goes.Goes
+}
 
 func (Command) String() string { return "reboot" }
 
@@ -23,8 +26,12 @@ func (Command) Apropos() lang.Alt {
 	}
 }
 
-func (Command) Main(args ...string) error {
+func (c *Command) Goes(g *goes.Goes) { c.g = g }
+
+func (c *Command) Main(args ...string) error {
 	kexec.Prepare()
+
+	c.g.Main("umount", "-a")
 
 	_ = syscall.Reboot(syscall.LINUX_REBOOT_CMD_KEXEC)
 
