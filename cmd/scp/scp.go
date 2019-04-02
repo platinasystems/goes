2e372d3e34c3cddfca454d285895eb4fa92c7e9e
Kevin Paul Herbert <kph@platinasystems.com>
2019-04-02 16:01:37 -0700

    scp: Implement server source and sink mode
    This is a server for the scp protocol. It does not implement
    the client side of the protocol presently. It is only for
    use via ssh from remote hosts.
    
    This protocol is not documented. The best reference I could find,
    for which this code is based, is:
    
    https://web.archive.org/web/20170215184048/https://blogs.oracle.com/janp/entry/how_the_scp_protocol_works
    
    Signed-off-by: Kevin Paul Herbert <kph@platinasystems.com>

--- cmd/scp/scp.go
+++ cmd/scp/scp.go
@@ -5,11 +5,21 @@
 package scp
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"syscall"
+	"time"
 
 	"github.com/platinasystems/flags"
 	"github.com/platinasystems/goes/cmd"
 	"github.com/platinasystems/goes/lang"
+	"github.com/platinasystems/log"
 )
 
 type Command struct{}
@@ -39,7 +49,8 @@ OPTIONS
 }
 
 func (Command) Main(args ...string) error {
-	flag, args := flags.New(args, "-f", "-t")
+	log.Print(args)
+	flag, args := flags.New(args, "-d", "-f", "-p", "-r", "-t")
 	i := 0
 	if flag.ByName["-f"] {
 		i++
@@ -56,16 +67,221 @@ func (Command) Main(args ...string) error {
 	if len(args) > 1 {
 		return fmt.Errorf("unexpected %v", args)
 	}
-	if flag.ByName["-f"] {
-		return sourceMode(args...)
+	isDir := false
+	exists := false
+	p := args[0]
+	s, err := os.Stat(p)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return err
+		}
+		if flag.ByName["-d"] {
+			return err
+		}
+		if strings.HasSuffix(p, string(os.PathSeparator)) {
+			return fmt.Errorf("%s: is directory", p)
+		}
+		if flag.ByName["-r"] {
+			isDir = true
+		}
+	} else {
+		exists = true
+		if (flag.ByName["-d"] || flag.ByName["-r"]) && s.IsDir() {
+			isDir = true
+		} else if !s.Mode().IsRegular() {
+			return fmt.Errorf("%s: is not a regular file", p)
+		}
 	}
-	return sinkMode(args...)
+	if flag.ByName["-f"] {
+		return sourceMode(flag, bufio.NewReader(os.Stdin), p)
+	}
+	return sinkMode(flag, exists, isDir, bufio.NewReader(os.Stdin), p)
 }
 
-func sourceMode(args ...string) error {
-	return fmt.Errorf("No source mode yet")
+func sourceMode(flag *flags.Flags, r *bufio.Reader, p string) error {
+	s, err := os.Stat(p)
+	if err != nil {
+		return err
+	}
+	if s.IsDir() {
+		dir, err := ioutil.ReadDir(p)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("D%#o %d %s\n", s.Mode()&os.ModePerm, 0, s.Name())
+		b, err := r.ReadByte()
+		if err != nil {
+			return err
+		}
+		if b != 0 {
+			return fmt.Errorf("Unexpected response %#x", b)
+		}
+		for _, f := range dir {
+			err := sourceMode(flag, r, filepath.Join(p, f.Name()))
+			if err != nil {
+				return err
+			}
+		}
+		fmt.Printf("E\n")
+		b, err = r.ReadByte()
+		if err != nil {
+			return err
+		}
+		if b != 0 {
+			return fmt.Errorf("Unexpected response %#x", b)
+		}
+		return nil
+	}
+	if flag.ByName["-p"] {
+		stat := s.Sys().(*syscall.Stat_t)
+		fmt.Printf("T%d 0 %d 0\n", stat.Mtim.Sec, stat.Atim.Sec)
+		b, err := r.ReadByte()
+		if err != nil {
+			return err
+			if b != 0 {
+				return fmt.Errorf("Unexpected response %#x", b)
+			}
+		}
+	}
+	fmt.Printf("C%#o %d %s\n", s.Mode()&os.ModePerm, s.Size(), s.Name())
+	b, err := r.ReadByte()
+	if err != nil {
+		return err
+	}
+	if b != 0 {
+		return fmt.Errorf("Unexpected response %#x", b)
+	}
+	buf, err := ioutil.ReadFile(p)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%s\x00", buf)
+
+	b, err = r.ReadByte()
+	if err != nil {
+		return err
+	}
+	if b != 0 {
+		return fmt.Errorf("Unexpected response %#x", b)
+	}
+	return nil
 }
 
-func sinkMode(args ...string) error {
-	return fmt.Errorf("No sink mode yet")
+func sinkMode(flag *flags.Flags, exists, isDir bool, r *bufio.Reader, p string) error {
+	dirStack := make([]string, 0)
+	var atime, mtime time.Time
+	fmt.Printf("\x00")
+	log.Print("Hi!")
+	s := bufio.NewScanner(r)
+	log.Print("You!")
+	for s.Scan() {
+		log.Print("Got something from scanner")
+		c := s.Text()
+		log.Print(c)
+		switch c[0] {
+		case 'C':
+			// Cmmmm <length> <filename>
+			f := strings.SplitN(c[1:], " ", 3)
+			log.Print(f)
+			m, err := strconv.ParseUint(f[0], 0, 32)
+			if err != nil {
+				return err
+			}
+			l, err := strconv.ParseUint(f[1], 0, 64)
+			if err != nil {
+				return err
+			}
+			fmt.Printf("\x00")
+			b := make([]byte, l+1)
+			_, err = io.ReadFull(r, b)
+			if err != nil {
+				return err
+			}
+			if b[l] != 0 {
+				return fmt.Errorf("Unexpected trailing byte %02x\n", b[l])
+			}
+			b = b[:l]
+			fn := ""
+			if isDir {
+				fn = filepath.Join(p, f[2])
+			} else {
+				fn = p
+			}
+			log.Print("Path ", p, "Opening ", fn)
+			err = ioutil.WriteFile(fn, b, os.FileMode(m))
+			if err != nil {
+				return err
+			}
+			if flag.ByName["-p"] {
+				err = os.Chtimes(fn, atime, mtime)
+				if err != nil {
+					return err
+				}
+			}
+			fmt.Printf("\x00")
+			log.Print("Success!")
+
+		case 'D':
+			// Dmmmm <length-ignore> <dirname>
+			f := strings.SplitN(c[1:], " ", 3)
+			log.Print(f)
+			m, err := strconv.ParseUint(f[0], 0, 32)
+			if err != nil {
+				return err
+			}
+			_, err = strconv.ParseUint(f[1], 0, 64)
+			if err != nil {
+				return err
+			}
+			fmt.Printf("\x00")
+			dirStack = append(dirStack, p)
+			if exists || len(dirStack) > 1 {
+				p = filepath.Join(p, f[2])
+			}
+			log.Print("D command creating ", p)
+			err = os.Mkdir(p, os.FileMode(m))
+			if err != nil {
+				return fmt.Errorf("mkdir %s: %s", p, err)
+			}
+		case 'E':
+			if len(dirStack) == 0 {
+				return fmt.Errorf("E command without matching D")
+			}
+			p = dirStack[len(dirStack)-1]
+			dirStack = dirStack[:len(dirStack)-1]
+			fmt.Printf("\x00")
+
+		case 'T':
+			//T<mtime> <mtime-usec> <atime> <atime-usec>
+			if !flag.ByName["-p"] {
+				return fmt.Errorf("T command without -p")
+			}
+			f := strings.SplitN(c[1:], " ", 4)
+			mtimeSec, err := strconv.ParseUint(f[0], 0, 64)
+			if err != nil {
+				return err
+			}
+			mtimeUsec, err := strconv.ParseUint(f[1], 0, 64)
+			if err != nil {
+				return err
+			}
+			atimeSec, err := strconv.ParseUint(f[2], 0, 64)
+			if err != nil {
+				return err
+			}
+			atimeUsec, err := strconv.ParseUint(f[3], 0, 64)
+			if err != nil {
+				return err
+			}
+			mtime = time.Unix(int64(mtimeSec), int64(mtimeUsec)*1000)
+			atime = time.Unix(int64(atimeSec), int64(atimeUsec)*1000)
+			log.Print("T command times ", mtime, " ", atime)
+			fmt.Printf("\x00")
+
+		default:
+			return fmt.Errorf("Unknown command %s", c)
+		}
+	}
+	log.Print("Done!")
+	return nil
 }
