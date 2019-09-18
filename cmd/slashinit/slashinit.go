a011a5c624c7d987c05cd27bd3280015171a36bc
Kevin Paul Herbert <kph@platinasystems.com>
2019-09-18 17:29:42 -0400

    slashinit: No need to set up filesystems in init()
    When we are called as /init, we don't need to worry about other
    init() functions needing default filesystems set up, etc, so just
    do this in the Main() function.
    
    This is groundwork to allow slashinit to be included in images while
    doing filesystem setup before slashinit() is run.
    
    Signed-off-by: Kevin Paul Herbert <kph@platinasystems.com>

--- cmd/slashinit/slashinit.go
+++ cmd/slashinit/slashinit.go
@@ -17,67 +17,6 @@ import (
 
 const zero = uintptr(0)
 
-func init() {
-	if os.Getpid() != 1 {
-		return
-	}
-	if os.Args[0] != "/init" {
-		return
-	}
-
-	for _, mnt := range []struct {
-		dir    string
-		dev    string
-		fstype string
-		mode   os.FileMode
-	}{
-		{"/dev", "devtmpfs", "devtmpfs", 0755},
-		{"/dev/pts", "devpts", "devpts", 0755},
-		{"/proc", "proc", "proc", 0555},
-		{"/sys", "sysfs", "sysfs", 0555},
-		{"/run", "tmpfs", "tmpfs", 0755},
-		{"/tmp", "tmpfs", "tmpfs", 01777},
-	} {
-		if _, err := os.Stat(mnt.dir); os.IsNotExist(err) {
-			err = os.Mkdir(mnt.dir, os.FileMode(mnt.mode))
-			if err != nil {
-				log.Print("err", mnt.dir, ": ", err)
-				continue
-			}
-		}
-		err := syscall.Mount(mnt.dev, mnt.dir, mnt.fstype, zero, "")
-		if err != nil {
-			log.Print("err", mnt.dir, ": ", err)
-		}
-	}
-	for _, ln := range []struct {
-		src, dst string
-	}{
-		{"../proc/self/fd/0", "/dev/stdin"},
-		{"../proc/self/fd/1", "/dev/stdout"},
-		{"../proc/self/fd/2", "/dev/stderr"},
-	} {
-		if _, err := os.Stat(ln.dst); os.IsNotExist(err) {
-			err = os.Symlink(ln.src, ln.dst)
-			if err != nil {
-				log.Print("err", "ln", ln.dst, "->", ln.src,
-					":", err)
-			}
-		}
-	}
-	for fd := 0; fd <= 2; fd++ {
-		err := syscall.Close(fd)
-		if err != nil {
-			log.Print("err", "console close", ":", err)
-		}
-		_, err = syscall.Open("/dev/kmsg", syscall.O_RDWR, 0)
-		if err != nil {
-			log.Print("err", "console reopen", ":", err)
-		}
-	}
-
-}
-
 type Command struct {
 	Hook   func() error
 	FsHook func() error
@@ -115,6 +54,12 @@ func (c *Command) Goes(g *goes.Goes) { c.g = g }
 func (*Command) Kind() cmd.Kind { return cmd.DontFork }
 
 func (c *Command) Main(_ ...string) error {
+	fmt.Printf("in /slashinit\n")
+
+	c.mountVirtualFilesystems()
+	c.makeStdioLinks()
+	c.redirectStdioKmsg()
+
 	if c.Hook != nil {
 		if err := c.Hook(); err != nil {
 			log.Print("Error from board hook", err)
@@ -127,7 +72,67 @@ func (c *Command) Main(_ ...string) error {
 			log.Print("Error from filesystem hook", err)
 		}
 	}
+	fmt.Printf("starting start\n")
 	return c.g.Main("start")
+}
+
+func (*Command) mountVirtualFilesystems() {
+	for _, mnt := range []struct {
+		dir    string
+		dev    string
+		fstype string
+		mode   os.FileMode
+	}{
+		{"/dev", "devtmpfs", "devtmpfs", 0755},
+		{"/dev/pts", "devpts", "devpts", 0755},
+		{"/proc", "proc", "proc", 0555},
+		{"/sys", "sysfs", "sysfs", 0555},
+		{"/run", "tmpfs", "tmpfs", 0755},
+		{"/tmp", "tmpfs", "tmpfs", 01777},
+	} {
+		if _, err := os.Stat(mnt.dir); os.IsNotExist(err) {
+			err = os.Mkdir(mnt.dir, os.FileMode(mnt.mode))
+			if err != nil {
+				log.Print("err", mnt.dir, ": ", err)
+				continue
+			}
+		}
+		err := syscall.Mount(mnt.dev, mnt.dir, mnt.fstype, zero, "")
+		if err != nil {
+			log.Print("err", mnt.dir, ": ", err)
+		}
+	}
+}
+
+func (*Command) makeStdioLinks() {
+	for _, ln := range []struct {
+		src, dst string
+	}{
+		{"../proc/self/fd/0", "/dev/stdin"},
+		{"../proc/self/fd/1", "/dev/stdout"},
+		{"../proc/self/fd/2", "/dev/stderr"},
+	} {
+		if _, err := os.Stat(ln.dst); os.IsNotExist(err) {
+			err = os.Symlink(ln.src, ln.dst)
+			if err != nil {
+				log.Print("err", "ln", ln.dst, "->", ln.src,
+					":", err)
+			}
+		}
+	}
+}
+
+func (*Command) redirectStdioKmsg() {
+	for fd := 0; fd <= 2; fd++ {
+		err := syscall.Close(fd)
+		if err != nil {
+			log.Print("err", "console close", ":", err)
+		}
+		_, err = syscall.Open("/dev/kmsg", syscall.O_RDWR, 0)
+		if err != nil {
+			log.Print("err", "console reopen", ":", err)
+		}
+	}
 }
 
 func (*Command) makeTargetDirs() {
