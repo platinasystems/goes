380911534167ff20182a2a792a9a4ce4666458ad
Kevin Paul Herbert <kph@platinasystems.com>
2020-08-03 12:03:43 -0400

    stop: Use daemons stop instead of sending SIGTERM ourselves
    If the stop script returns an error, treat as a warning and continue
    with the shutdown.
    
    Instead of sending SIGTERM (and then SIGKILL) to stop daemons (which we
    identify as any process matching our /proc/self/exe), use the
    "daemons stop" command to stop the daemons via RPC.
    
    Signed-off-by: Kevin Paul Herbert <kph@platinasystems.com>

--- cmd/stop/stop.go
+++ cmd/stop/stop.go
@@ -9,14 +9,11 @@ package stop
 import (
 	"fmt"
 	"os"
-	"strings"
-	"syscall"
 
 	"github.com/platinasystems/goes"
 	"github.com/platinasystems/goes/cmd"
 	"github.com/platinasystems/goes/external/parms"
 	"github.com/platinasystems/goes/internal/assert"
-	"github.com/platinasystems/goes/internal/kill"
 	"github.com/platinasystems/goes/lang"
 )
 
@@ -71,25 +68,12 @@ func (c *Command) Main(args ...string) error {
 	if len(stop) > 0 {
 		err = c.g.Main("source", stop)
 		if err != nil {
-			return fmt.Errorf("source %s: %v", stop, err)
+			fmt.Printf("source %s: %s\n", stop, err)
 		}
 	}
-	sig := syscall.SIGTERM
-	if len(args) == 1 {
-		var found bool
-		sig, found = sigByName[strings.ToUpper(args[0])]
-		if !found {
-			return fmt.Errorf("signal: %s: unknown", args[0])
-		}
-	}
-	err = kill.All(sig)
-	if c.Hook != nil {
-		if t := c.Hook(); err != nil || t != nil {
-			if err != nil {
-				err = t
-			}
-			kill.All(syscall.SIGKILL)
-		}
+	err = c.g.Main("daemons", "stop")
+	if err != nil {
+		fmt.Printf("Error from daemons stop: %s\n", err)
 	}
 	return err
 }
@@ -97,42 +81,4 @@ func (c *Command) Main(args ...string) error {
 func haveEtcGoesStop() bool {
 	_, err := os.Stat(EtcGoesStop)
 	return err == nil
-}
-
-var sigByName = map[string]syscall.Signal{
-	"SIGABR":    syscall.SIGABRT,
-	"SIGALRM":   syscall.SIGALRM,
-	"SIGBUS":    syscall.SIGBUS,
-	"SIGCHLD":   syscall.SIGCHLD,
-	"SIGCLD":    syscall.SIGCLD,
-	"SIGCONT":   syscall.SIGCONT,
-	"SIGFPE":    syscall.SIGFPE,
-	"SIGHUP":    syscall.SIGHUP,
-	"SIGILL":    syscall.SIGILL,
-	"SIGINT":    syscall.SIGINT,
-	"SIGIO":     syscall.SIGIO,
-	"SIGIOT":    syscall.SIGIOT,
-	"SIGKILL":   syscall.SIGKILL,
-	"SIGPIPE":   syscall.SIGPIPE,
-	"SIGPOLL":   syscall.SIGPOLL,
-	"SIGPROF":   syscall.SIGPROF,
-	"SIGPWR":    syscall.SIGPWR,
-	"SIGQUIT":   syscall.SIGQUIT,
-	"SIGSEGV":   syscall.SIGSEGV,
-	"SIGSTKFLT": syscall.SIGSTKFLT,
-	"SIGSTOP":   syscall.SIGSTOP,
-	"SIGSYS":    syscall.SIGSYS,
-	"SIGTERM":   syscall.SIGTERM,
-	"SIGTRAP":   syscall.SIGTRAP,
-	"SIGTSTP":   syscall.SIGTSTP,
-	"SIGTTIN":   syscall.SIGTTIN,
-	"SIGTTOU":   syscall.SIGTTOU,
-	"SIGUNUSED": syscall.SIGUNUSED,
-	"SIGURG":    syscall.SIGURG,
-	"SIGUSR1":   syscall.SIGUSR1,
-	"SIGUSR2":   syscall.SIGUSR2,
-	"SIGVTALRM": syscall.SIGVTALRM,
-	"SIGWINCH":  syscall.SIGWINCH,
-	"SIGXCPU":   syscall.SIGXCPU,
-	"SIGXFSZ":   syscall.SIGXFSZ,
 }
