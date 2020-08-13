b3abdc15b5555bb43bb4dbe6e38481f76bb6b802
Kevin Paul Herbert <kph@platinasystems.com>
2020-08-13 14:34:24 -0700

    mountd: Track mounts and mount errors and unmount what we mounted
    Unmount what we mount. Don't retry partitions that cause errors.
    
    Signed-off-by: Kevin Paul Herbert <kph@platinasystems.com>

--- cmd/mountd/mountd.go
+++ cmd/mountd/mountd.go
@@ -22,21 +22,23 @@ import (
 
 var ErrUnknownPartition = errors.New("Unable to determine partition type")
 
-type Command struct{}
+type Command struct {
+	partitions map[string]error
+}
 
-func (Command) String() string { return "mountd" }
+func (*Command) String() string { return "mountd" }
 
-func (Command) Usage() string { return "mountd" }
+func (*Command) Usage() string { return "mountd" }
 
-func (Command) Apropos() lang.Alt {
+func (*Command) Apropos() lang.Alt {
 	return lang.Alt{
 		lang.EnUS: "mount daemon",
 	}
 }
 
-func (Command) Kind() cmd.Kind { return cmd.Daemon }
+func (*Command) Kind() cmd.Kind { return cmd.Daemon }
 
-func (Command) mountone(dev, dir string) (err error) {
+func (*Command) mountone(dev, dir string) (err error) {
 	sb, err := partitions.ReadSuperBlock(dev)
 	if err != nil {
 		return err
@@ -58,43 +60,83 @@ func (Command) mountone(dev, dir string) (err error) {
 	return err
 }
 
-func (c Command) mountall(mp string) {
+func (c *Command) mountall(mp string) {
 	pp, err := os.Open("/proc/partitions")
 	if err != nil {
 		log.Printf("opening /proc/partitions: %s", err)
 		return
 	}
 	defer pp.Close()
-	scanner := bufio.NewScanner(pp)
-scan:
-	for scanner.Scan() {
-		line := scanner.Text()
-		fields := strings.Fields(line)
+	partMap := make(map[string]bool)
+	partScanner := bufio.NewScanner(pp)
+
+	for partScanner.Scan() {
+		fields := strings.Fields(partScanner.Text())
 		if len(fields) < 4 || fields[0] == "major" {
-			continue scan
+			continue
 		}
-		fileName := fields[3]
-		mpd := mp + "/" + fileName
-		if _, err := os.Stat(mpd); os.IsNotExist(err) {
-			err := os.Mkdir(mpd, os.FileMode(0555))
-			if err != nil {
-				fmt.Println("mkdir", mpd, "err:", err)
-			} else {
-				err := c.mountone("/dev/"+fileName, mpd)
-				if err == nil {
-					fmt.Println("Mounted", "/dev/"+fileName,
-						mpd)
-				} else {
-					if err != ErrUnknownPartition {
-						fmt.Println("mount", mpd, "err:", err)
-					}
+		partMap[fields[3]] = true
+	}
+
+	pm, err := os.Open("/proc/mounts")
+	if err != nil {
+		fmt.Printf("opening /proc/mounts: %s\n", err)
+		return
+	}
+	defer pm.Close()
+
+	mountMap := make(map[string]string)
+	mountScanner := bufio.NewScanner(pm)
+	for mountScanner.Scan() {
+		fields := strings.Fields(mountScanner.Text())
+		if len(fields) < 6 {
+			continue
+		}
+		mountMap[fields[0]] = fields[1]
+	}
+
+	for part := range partMap {
+		mpd := mp + "/" + part
+		if _, found := c.partitions[mpd]; !found {
+			if _, err := os.Stat(mpd); os.IsNotExist(err) {
+				err := os.Mkdir(mpd, os.FileMode(0555))
+				if err != nil {
+					fmt.Println("mkdir", mpd, "err:", err)
+					continue
 				}
+			}
+			err := c.mountone("/dev/"+part, mpd)
+			if err == nil {
+				fmt.Println("Mounted", "/dev/"+part,
+					mpd)
+			} else {
+				if err != ErrUnknownPartition {
+					fmt.Println("mount", mpd, "err:", err)
+				}
+			}
+			c.partitions[mpd] = err
+		} else {
+			if mountMap["/dev/"+part] != mpd &&
+				c.partitions[mpd] == nil {
+				delete(c.partitions, mpd)
 			}
 		}
 	}
 }
 
-func (c Command) Main(args ...string) (err error) {
+func (c *Command) unmountall(mp string) {
+	for mpd, e := range c.partitions {
+		if e == nil {
+			err := syscall.Unmount(mpd, syscall.MNT_DETACH)
+			if err != nil {
+				fmt.Printf("Error unmounting %s: %s\n", mpd, err)
+			}
+		}
+		delete(c.partitions, mpd)
+	}
+}
+
+func (c *Command) Main(args ...string) (err error) {
 	mp := "/mountd"
 	if len(args) > 0 {
 		mp = args[0]
@@ -105,6 +147,7 @@ func (c Command) Main(args ...string) (err error) {
 			log.Print("err", mp, ": ", err)
 		}
 	}
+	c.partitions = make(map[string]error)
 	c.mountall(mp)
 	t := time.NewTicker(2 * time.Second)
 	defer t.Stop()
@@ -112,6 +155,7 @@ func (c Command) Main(args ...string) (err error) {
 		c.mountall(mp)
 		select {
 		case <-goes.Stop:
+			c.unmountall(mp)
 			return nil
 		case <-t.C:
 		}
