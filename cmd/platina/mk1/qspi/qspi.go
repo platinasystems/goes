1440e560d980a3f9b8c9c210fcf3e7d64ae7a04a
Kevin Paul Herbert <kph@platinasystems.com>
2021-02-04 18:22:03 -0800

    qspi: Add man page, -watchdog option, and fix issues
    Add a man page.
    
    Add the -watchdog option to enable the watchdog.
    
    Read the status from 0x602.
    
    Improve error messages.
    
    Don't change the QSPI in the same operation as changing the watchdog
    flags.
    
    Signed-off-by: Kevin Paul Herbert <kph@platinasystems.com>

--- cmd/platina/mk1/qspi/qspi.go
+++ cmd/platina/mk1/qspi/qspi.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"strconv"
 
+	"github.com/platinasystems/goes/external/flags"
 	"github.com/platinasystems/goes/lang"
 	"github.com/platinasystems/ioport"
 )
@@ -30,27 +31,41 @@ func (Command) Man() lang.Alt {
 	return lang.Alt{
 		lang.EnUS: `
 DESCRIPTION
-	The qspi command reports or sets the active QSPI device.`,
+	The qspi command reports or sets the active QSPI device.
+
+USAGE
+	qspi [-watchdog] unit
+
+	-watchdog	Enable the watchdog timer
+
+	Specify the primary QSPI as 0 and the backup as 1.`,
 	}
 }
 
 func (c Command) Main(args ...string) (err error) {
+	flag, args := flags.New(args,
+		"-watchdog",
+	)
+
 	sel := 0
-	v, err := ioport.Inb(0x604)
+	s1, err := ioport.Inb(0x602)
 	if err != nil {
-		return fmt.Errorf("Error in Inb(0x604): %s", err)
+		return fmt.Errorf("Error reading CPLD Status-1: %s", err)
 	}
-	if v&0x80 != 0 {
+	if s1&0x80 != 0 {
 		sel = 1
 	}
 
 	if len(args) == 0 {
 		fmt.Printf("QSPI%d is selected\n", sel)
-		return nil
+		if !flag.ByName["-watchdog"] {
+			return nil
+		}
 	}
 
+	req := sel
 	if len(args) > 0 {
-		sel, err = strconv.Atoi(args[0])
+		req, err = strconv.Atoi(args[0])
 		if err != nil {
 			return fmt.Errorf("Error parsing %s: %s", args[0], err)
 		}
@@ -58,20 +73,38 @@ func (c Command) Main(args ...string) (err error) {
 		if len(args) > 0 {
 			return fmt.Errorf("%v: unexpected", args)
 		}
-		if sel < 0 || sel > 1 {
-			return fmt.Errorf("Invalid unit number %d", sel)
+		if req < 0 || req > 1 {
+			return fmt.Errorf("Invalid unit number %d", req)
 		}
 	}
 
-	v = (v & 0x7f) | byte(sel<<7)
-
-	err = ioport.Outb(0x604, v)
+	c1, err := ioport.Inb(0x604)
 	if err != nil {
-		return fmt.Errorf("Error in outb(0x604): %s\n",
+		return fmt.Errorf("Error reading CPLD Ctrl-1: %s", err)
+	}
+	c1 = (c1 & 0x7c) | byte(sel<<7)
+	err = ioport.Outb(0x604, c1)
+	if err != nil {
+		return fmt.Errorf("Error clearing watchdog in CPLD Ctrl-1: %s\n",
 			err)
 	}
 
-	fmt.Printf("Selected QSPI%d\n", sel)
+	if flag.ByName["-watchdog"] {
+		c1 |= 0x3
+		err = ioport.Outb(0x604, c1)
+		if err != nil {
+			return fmt.Errorf("Error setting watchdog in CPLD Ctrl-1: %s\n",
+				err)
+		}
+	}
+	c1 = (c1 & 0x7f) | byte(req<<7)
+	err = ioport.Outb(0x604, c1)
+	if err != nil {
+		return fmt.Errorf("Error selecting QSPI in CPLD Ctrl-1: %s\n",
+			err)
+	}
+
+	fmt.Printf("Selected QSPI%d\n", req)
 
 	return nil
 }
