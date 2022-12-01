352d14f0a36d0b600787df82fd630d342c7c3330
Tom Grennan <tmgrennan@gmail.com>
2022-11-30 17:43:01 -0800

    add host.Ner* swap
    Signed-off-by: Tom Grennan <tmgrennan@gmail.com>

--- v2/pkg/encoding/binary/host/host_big.go
+++ v2/pkg/encoding/binary/host/host_big.go
@@ -8,9 +8,18 @@ package host
 
 import "github.com/platinasystems/goes/v2/pkg/encoding/binary/big"
 
+const (
+	IsBigEndian    = true
+	IsLittleEndian = false
+)
+
 type Uint8 = big.Uint8
 type Uint16 = big.Uint16
 type Uint32 = big.Uint32
 type Uint64 = big.Uint64
 type Float32 = big.Float32
 type Float64 = big.Float64
+
+func Net16(v uint16) uint16 { return v }
+func Net32(v uint32) uint32 { return v }
+func Net64(v uint64) uint64 { return v }
--- v2/pkg/encoding/binary/host/host_little.go
+++ v2/pkg/encoding/binary/host/host_little.go
@@ -8,9 +8,26 @@ package host
 
 import "github.com/platinasystems/goes/v2/pkg/encoding/binary/little"
 
+const (
+	IsBigEndian    = false
+	IsLittleEndian = true
+)
+
 type Uint8 = little.Uint8
 type Uint16 = little.Uint16
 type Uint32 = little.Uint32
 type Uint64 = little.Uint64
 type Float32 = little.Float32
 type Float64 = little.Float64
+
+func Net16(v uint16) uint16 {
+	return (v << 8) | ((v >> 8) & 255)
+}
+
+func Net32(v uint32) uint32 {
+	return (uint32(Net16(uint16(v))) << 16) | uint32(Net16(uint16(v>>16)))
+}
+
+func Net64(v uint64) uint64 {
+	return (uint64(Net32(uint32(v))) << 32) | uint64(Net32(uint32(v>>32)))
+}
