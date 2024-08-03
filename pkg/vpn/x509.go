77398f42c60f03eb261e358f6169dc97504c299c
Tom Grennan <tmgrennan@gmail.com>
2024-08-02 19:14:32 -0700

    vpn: check peer signature with matching subscriber
    Signed-off-by: Tom Grennan <tmgrennan@gmail.com>

--- pkg/vpn/x509.go
+++ pkg/vpn/x509.go
@@ -343,6 +343,13 @@ func (cs *Certificates) First() *x509.Certificate {
 func (cs *Certificates) Has(peer *x509.Certificate) bool {
 	for _, bc := range cs.BCs {
 		if bc.Cert != nil && bc.Cert.Equal(peer) {
+			err := bc.Cert.CheckSignature(peer.SignatureAlgorithm,
+				peer.RawTBSCertificate,
+				peer.Signature)
+			if err != nil {
+				verbose.Println(peer.Subject.CommonName, err)
+				return false
+			}
 			return true
 		}
 	}
