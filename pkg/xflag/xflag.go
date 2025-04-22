f780ee11c07d8a40dab60b771ee12d90b9abbd38
Tom Grennan <tgrennan@platinasystems.com>
2025-04-22 16:13:44 -0700

    xflag: generic KeyUsage to def and get flag values
    Signed-off-by: Tom Grennan <tgrennan@platinasystems.com>

--- pkg/xflag/xflag.go
+++ pkg/xflag/xflag.go
@@ -5,11 +5,19 @@
 package xflag
 
 import (
+	"encoding"
+	"errors"
 	"flag"
 	"fmt"
+	"reflect"
 	"strings"
 	"text/template"
+	"time"
+	"unicode"
 )
+
+var ErrIsUndefinable = errors.New("is undefinable")
+var ErrIsNotTextUnmarshaler = errors.New("is not TextUnmarshaler")
 
 func Get[T any](name string) T {
 	return GetFrom[T](flag.CommandLine, name)
@@ -22,6 +30,45 @@ func GetFrom[T any](flags *flag.FlagSet, name string) (v T) {
 		}
 	}
 	return
+}
+
+// A [unicode.Space] separated flag key and usage, e.g.
+//
+//	const VerboseFlag KeyUsage[bool] = "v Verbose output."
+type KeyUsage[T any] string
+
+func (ku KeyUsage[T]) Define(val T, aliases ...string) *T {
+	return ku.DefineIn(flag.CommandLine, val, aliases...)
+}
+
+func (ku KeyUsage[T]) DefineIn(flags *flag.FlagSet, val T, aliases ...string) *T {
+	var key, usage string
+	for i, r := range string(ku) {
+		if len(key) == 0 {
+			if unicode.IsSpace(r) {
+				key = string(ku)[:i]
+			}
+		} else if !unicode.IsSpace(r) {
+			usage = string(ku)[i:]
+			break
+		}
+	}
+	return define(flags, key, val, usage, aliases...).(*T)
+}
+
+func (ku KeyUsage[T]) Value() T {
+	return ku.ValueIn(flag.CommandLine)
+}
+
+func (ku KeyUsage[T]) ValueIn(flags *flag.FlagSet) T {
+	var key string
+	for i, r := range string(ku) {
+		if unicode.IsSpace(r) {
+			key = string(ku)[:i]
+			break
+		}
+	}
+	return GetFrom[T](flags, key)
 }
 
 func LastName(flags *flag.FlagSet) string {
@@ -73,4 +120,108 @@ func UsageTemplate(flags *flag.FlagSet, tmpl string) {
 			fmt.Fprint(w, name, ":usage:", err, "\n")
 		}
 	}
+}
+
+func define(flags *flag.FlagSet, name string, val any, usage string,
+	aliases ...string,
+) any {
+	switch t := val.(type) {
+	case bool:
+		p := flags.Bool(name, t, usage)
+		if len(aliases) > 0 {
+			aka := fmt.Sprint("aka -", name)
+			for _, alias := range aliases {
+				flags.BoolVar(p, alias, *p, aka)
+			}
+		}
+		return p
+	case time.Duration:
+		p := flags.Duration(name, t, usage)
+		if len(aliases) > 0 {
+			aka := fmt.Sprint("aka -", name)
+			for _, alias := range aliases {
+				flags.DurationVar(p, alias, *p, aka)
+			}
+		}
+		return p
+	case float64:
+		p := flags.Float64(name, t, usage)
+		if len(aliases) > 0 {
+			aka := fmt.Sprint("aka -", name)
+			for _, alias := range aliases {
+				flags.Float64Var(p, alias, *p, aka)
+			}
+		}
+		return p
+	case int:
+		p := flags.Int(name, t, usage)
+		if len(aliases) > 0 {
+			aka := fmt.Sprint("aka -", name)
+			for _, alias := range aliases {
+				flags.IntVar(p, alias, *p, aka)
+			}
+		}
+		return p
+	case int64:
+		p := flags.Int64(name, t, usage)
+		if len(aliases) > 0 {
+			aka := fmt.Sprint("aka -", name)
+			for _, alias := range aliases {
+				flags.Int64Var(p, alias, *p, aka)
+			}
+		}
+		return p
+	case string:
+		p := flags.String(name, t, usage)
+		if len(aliases) > 0 {
+			aka := fmt.Sprint("aka -", name)
+			for _, alias := range aliases {
+				flags.StringVar(p, alias, *p, aka)
+			}
+		}
+		return p
+	case uint:
+		p := flags.Uint(name, t, usage)
+		if len(aliases) > 0 {
+			aka := fmt.Sprint("aka -", name)
+			for _, alias := range aliases {
+				flags.UintVar(p, alias, *p, aka)
+			}
+		}
+		return p
+	case uint64:
+		p := flags.Uint64(name, t, usage)
+		if len(aliases) > 0 {
+			aka := fmt.Sprint("aka -", name)
+			for _, alias := range aliases {
+				flags.Uint64Var(p, alias, *p, aka)
+			}
+		}
+		return p
+	case encoding.TextMarshaler:
+		tum, ok := t.(encoding.TextUnmarshaler)
+		if !ok {
+			rval := reflect.ValueOf(t)
+			if rval.Kind() == reflect.Ptr {
+				rval = reflect.Indirect(rval)
+			}
+			p := reflect.New(rval.Type()).Interface()
+			tum, ok = p.(encoding.TextUnmarshaler)
+			if !ok {
+				panic(fmt.Errorf("%T %w", t,
+					ErrIsNotTextUnmarshaler))
+			}
+		}
+		flags.TextVar(tum, name, t, usage)
+		if len(aliases) > 0 {
+			aka := fmt.Sprint("aka -", name)
+			for _, alias := range aliases {
+				flags.TextVar(tum, alias, t, aka)
+			}
+		}
+		return tum
+	default:
+		panic(fmt.Errorf("%T %w", t, ErrIsUndefinable))
+	}
+	return nil
 }
