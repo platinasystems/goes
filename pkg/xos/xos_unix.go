733148345cb4fc9b0b16f04a437d02df472abeeb
Tom Grennan <tgrennan@platinasystems.com>
2025-05-04 13:58:21 -0700

    xos: add SetNonblock and IsBlocked
    Signed-off-by: Tom Grennan <tgrennan@platinasystems.com>

--- pkg/xos/xos_unix.go
+++ pkg/xos/xos_unix.go
@@ -1,4 +1,4 @@
-// Copyright © 2015-2024 Platina Systems, Inc. All rights reserved.
+// Copyright © 2015-2025 Platina Systems, Inc. All rights reserved.
 // Use of this source code is governed by the GPL-2 license described in the
 // LICENSE file.
 
@@ -7,9 +7,18 @@
 package xos
 
 import (
+	"errors"
 	. "os"
 
 	"golang.org/x/sys/unix"
 )
 
 var Termination = []Signal{Interrupt, Signal(unix.SIGTERM)}
+
+func IsBlocked(err error) bool {
+	return errors.Is(err, unix.EAGAIN) || errors.Is(err, unix.EWOULDBLOCK)
+}
+
+func SetNonblockFile(f *File, val bool) error {
+	return unix.SetNonblock(int(f.Fd()), val)
+}
