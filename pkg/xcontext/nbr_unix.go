6498e252acb4649cacf5394bd48b560d00dfdf43
Tom Grennan <tgrennan@platinasystems.com>
2025-05-08 15:43:31 -0700

    xcontext: remove unused Redirect
    Signed-off-by: Tom Grennan <tgrennan@platinasystems.com>

--- pkg/xcontext/nbr_unix.go
+++ pkg/xcontext/nbr_unix.go
@@ -1,22 +1,17 @@
-// Copyright © 2022-2024 Platina Systems, Inc. All rights reserved.
+// Copyright © 2022-2025 Platina Systems, Inc. All rights reserved.
 // Use of this source code is governed by the GPL-2 license described in the
 // LICENSE file.
 
 package xcontext
 
 import (
-	"bufio"
 	"context"
 	"errors"
-	"io"
 	"os"
-	"sync"
 	"time"
 
 	"golang.org/x/sys/unix"
 )
-
-type Printer interface{ Print(...any) }
 
 // Non-Blocking Reader
 type NBR struct {
@@ -49,28 +44,5 @@ func (nbr NBR) Read(b []byte) (int, error) {
 			}
 			return 0, context.Canceled
 		}
-	}
-}
-
-// Redirect opens an os.Pipe and, after starting a goroutine to Print the pipe
-// input, returns the pipe output. The goroutine continues until context done
-// and the returned writer is an *os.File that may reassign os.Stdout or
-// os.Strerr.
-func Redirect(ctx context.Context, wg *sync.WaitGroup, p Printer) *os.File {
-	r, w, err := os.Pipe()
-	if err != nil {
-		panic(err)
-	}
-
-	wg.Add(1)
-	go goredirect(wg, p, WithNBR(ctx, r))
-	return w
-}
-
-func goredirect(wg *sync.WaitGroup, p Printer, r io.Reader) {
-	defer wg.Done()
-	scanner := bufio.NewScanner(r)
-	for scanner.Scan() {
-		p.Print(scanner.Text())
 	}
 }
